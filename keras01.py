db91feb62bcc63545835c618d2ff1161dd6fd38a
jieun-kim-debug <64455878+jieun-kim-debug@users.noreply.github.com>
2020-05-12 10:11:06 +0900

    Update keras01.py

--- keras01.py
+++ keras01.py
@@ -21,5 +21,23 @@ loss, acc = model.evaluate(x, y, batch_size=1)
 print("loss : ", loss)
 print("acc : ", acc)
 
+#epochs=500, batch_size=1, Dense=1
 #loss : 1.10..(2nd 훈련)
 #acc : 1.0
+
+#epochs=1000, batch_size=1, Dense=1
+#loss : 5.31..(1st 훈련) / 0.0(3rd 훈련)
+#acc : 1.0
+
+#epochs=5000, batch_size=10, Dense=1
+#loss : 38.5..(1st 훈련)
+#acc : 0.0
+#왜 실패지?
+
+#epochs=500, batch_size=2, Dense=1
+#loss : 1.11..(1st 훈련) / 38.5(2nd 훈련) / 0.057..(3rd)
+#acc : 1.0 (1st) / 0.0 (2nd) / 1.0 (3rd)
+
+#epochs=500, batch_size=10, Dense=1
+#loss : 38.5 (1st) / 9.010..(2nd 훈련) / 0.155..(3rd) / 38.5(4th)
+#acc : 1.0 (1st) / 0.1 (2nd) / 0.7 (3rd) / 0.0 (4th)

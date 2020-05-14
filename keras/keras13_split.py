733c7344d9c8221c823eece429b092a036d83d2b
jieun-kim-debug <64455878+jieun-kim-debug@users.noreply.github.com>
2020-05-14 19:09:07 +0900

    Update keras13_split.py

--- keras/keras13_split.py
+++ keras/keras13_split.py
@@ -1,8 +1,8 @@
 # 과제 train_test_split 함수를 1번만 써서 model.fit에서 validation_data를 다른 파라미터로 대체해보시오
 # 1) train_test_split 함수 2개 중 x_val, y_val이 포함된 두번째 함수 삭제
-# 2) validation_data=(x_val, y_val)에 x_val, y_val를 삭제했기 때문에 쓸수가없음. 다른 파라미터로 대체
+# 2) model.fit validation_data=(x_val, y_val)에 x_val, y_val를 삭제했기 때문에 쓸수가없음. 다른 파라미터로 대체
 # 3) 그럼 어떤 파라미터? 구글링에 소질이 없나봄.. 꿀팁! model.fit validation_까지만 치면 연관검색어에 model.fit validation_data 바로 밑에 split이 나옴
-# 4) model.fit validation_split를 쓰면 train 비율 X split 비율을 하면 val의 비율과 train의 비율이 나옴
+# 4) model.fit validation_split를 쓰면 train 비율에 validation 비율을 split 하는 것임
 # 이게 가능한 이유는 train에 validation 검증이 포함되어 있기 때문임
 
 #1. 데이터
@@ -61,7 +61,6 @@ print(y_predict)
 
 # RMSE 구하기
 from sklearn.metrics import mean_squared_error
-
 
 def RMSE(y_test, y_predict): 
     return np.sqrt(mean_squared_error(y_test, y_predict)) 

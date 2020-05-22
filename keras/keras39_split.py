a83fc1dbdf4bb5e8f64925c596afee9b6be234fd
jieun-kim-debug <64455878+jieun-kim-debug@users.noreply.github.com>
2020-05-22 19:09:40 +0900

    Update keras39_split.py

--- keras/keras39_split.py
+++ keras/keras39_split.py
@@ -6,6 +6,8 @@ from keras.layers import Dense, LSTM
 a = np.array(range(1,11))
 size = 5
 
+# 코드 분석 시, 처음보는 변수명이나 매개변수가 나와도 당황하지 말자
+# 코드 안에 반드시 존재한다. 그렇지 않으면 구글링링링링마벨
 def split_x(seq, size):
     aaa = []
     for i in range(len(seq) - size + 1):

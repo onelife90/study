903d12082ecb48717888a45941366380e8312c3e
jieun-kim-debug <64455878+jieun-kim-debug@users.noreply.github.com>
2020-05-23 23:08:37 +0900

    Create p056_scatterplot_axes.py

---
+++ homework/p056_scatterplot_axes.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt
+
+test_1_grades = [99,90,85,97,80]
+test_2_grades = [100,85,60,90,70]
+
+# matplotlib에게 자동으로 축의 범위를 설정한 경우
+plt.scatter(test_1_grades, test_2_grades)
+plt.title("Axes Aren't Compaarable")
+plt.xlabel("test 1 grade")
+plt.ylabel("test 2 grade")
+plt.show()

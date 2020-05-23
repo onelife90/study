490c8002a6cbadfdbe1082f3acecffcbbea1d00e
jieun-kim-debug <64455878+jieun-kim-debug@users.noreply.github.com>
2020-05-23 12:16:49 +0900

    Update p004_dictionary.py

--- homework/p004_dictionary.py
+++ homework/p004_dictionary.py
@@ -17,3 +17,19 @@ friendship_pairs = [(0,1), (0,2), (1,2), (1,3), (2,3), (3,4),
 friendships = {user["id"]: [] for user in users}
 
 for i, j in friendship_pairs:
+friendships[i].append(j)
+friendships[j].append(i)
+
+def number_of_friends(user):
+  """user의 친구는 몇 명일까?"""
+  user_id = user["id"]
+  friends_ids = friendships[user_id]
+  return len(friends_ids)
+
+total_connections = sum(number_of_friends(user)
+                        for user in users)
+
+# 친구가 가장 많은 사람을 알아보자. 오름차순으로 사용자를 정렬.
+num_users = len(users)
+avg_connections = total_connections / num_users
+

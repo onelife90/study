ed77c3d44f3a301d47492e49bc430c526d893658
jieun-kim-debug <64455878+jieun-kim-debug@users.noreply.github.com>
2020-05-23 19:47:45 +0900

    Update p026_dictionary.py

--- homework/p026_dictionary.py
+++ homework/p026_dictionary.py
@@ -6,3 +6,7 @@ try:
     kates_grade = grades["Kate"]    # 딕셔너리에 존재하지 않는 키를 입력하면 KeyError가 발생
 except KeyError:
     print("no grade for Kate!")
+
+from collections import defaultdict
+dd_dict = defaultdict(dict)             # dict()는 빈 딕셔너리를 생성
+dd_dict["Joel"]["City"] = "Seattle"     # {"Joel" : {"City": Seattle"}}     # 딕셔너리 안에 딕셔너리 가능

6fa62108681840df04fb282953fa86b06adde23c
jieun-kim-debug <64455878+jieun-kim-debug@users.noreply.github.com>
2020-05-25 13:40:26 +0900

    Update keras42_lstm_split2.py

--- keras/keras42_lstm_split2.py
+++ keras/keras42_lstm_split2.py
@@ -16,31 +16,32 @@ def split_x(seq, size):
     # print(type(aaa))
     return np.array(aaa)
 
-dataset = split_x(a, size)      # (96,5)
+dataset = split_x(a, size)      
 # print(dataset)
-# print(dataset.shape)
+# print(dataset.shape)        # (96,5)
 # print(type(dataset))        # numpy.ndarray
 # 왜? split_x 함수에서 리턴을 np.array로 했기 때문에
 
 x = dataset[:, 0:4]     # [행, 열] = [: all 모든 값, 0:4] 
 y = dataset[:, 4]
 
-x_predict = dataset[-6,-6]
-# print(x)
-# print(y)
+x_predict = dataset[90:, 0:4]
+print(x)
+print(y)
+print(x_predict)
 
-x = np.reshape(x, (6,4,1))
-# x = x.reshape(6,4,1)과 같은 표현
+x = x.reshape(x.shape[0],x.shape[1],1)
+print(x.shape)
+x_predict = x_predict.reshape(6,4,1)
 
 #실습 1. train, test 분리할 것 (8:2)
 #실습 2. 마지막 6개의 행을 predict로 만들고 싶다
 #실습 3. validation을 넣을 것 (train의 20%)
 
-from sklearn.
+from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(
-    # x,y, random_state=99, shuffle=True,
     x,y, train_size=0.8
-)  
+    )  
 
 #2. 모델구성
 
@@ -59,26 +60,27 @@ from keras.callbacks import EarlyStopping
 early_stopping = EarlyStopping(monitor='loss', patience=5, mode='auto')
 
 model.compile(loss = 'mse', optimizer='adam', metrics=['mse'])
-model.fit(x,y, epochs=1000, batch_size=1, verbose=1,
-        callbacks=[early_stopping])
+model.fit(x_train, y_train, epochs=1000, batch_size=1, verbose=1,
+        validation_split=0.2, shuffle=True, callbacks=[early_stopping])
 # model.fit의 batch_size와 x데이터의 batch_size는 다르다
 # x데이터의 batch_size는 총 6행으로 자르겠다는 의미이고
 # model.fit의 batch_size는 그 6행을 하나씩 자르겠다는 의미
 
 #4. 평가, 예측
-loss, mse = model.evaluate(x,y, batch_size=1)
-y_predict = model.predict(x)
+loss, mse = model.evaluate(x_test,y_test, batch_size=1)
+y_predict = model.predict(x_predict)
 
 print('loss: ', loss)
 print('mse: ', mse)
 print('y_predict: \n', y_predict)
 
-# loss:  0.0026065367796945793
-# mse:  0.0026065369602292776
+# epochs=38 노드=8,3,1,3,2,4,2,1
+# loss:  0.8519117459654808
+# mse:  0.8519118428230286
 # y_predict:
-#  [[5.0208592]
-#  [6.0008645]
-#  [6.9798226]
-#  [8.040006 ]
-#  [9.089726 ]
-#  [9.928272 ]]
+#  [[93.35192 ]
+#  [94.1267  ]
+#  [94.88074 ]
+#  [95.61367 ]
+#  [96.32529 ]
+#  [97.015366]]

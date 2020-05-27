787a9ac5bb74479111a50f59d714f38b80eda1dc
jieun-kim-debug <64455878+jieun-kim-debug@users.noreply.github.com>
2020-05-27 11:59:49 +0900

    Update keras49_classify2.py

--- keras/keras49_classify2.py
+++ keras/keras49_classify2.py
@@ -13,25 +13,19 @@ x_predict = np.array([1,2,3])
 # print(x.shape)      # (10,)
 # print(y.shape)      # (10,)
 
-# one-hot 인코딩
-# 0 1 2 3 4 5
-#[0 1 0 0 0 0] == 1
-#[0 0 1 0 0 0] == 2
-#[0 0 0 1 0 0] == 3
-#[0 0 0 0 1 0] == 4
-#[0 0 0 0 0 1] == 5
-# 각 레이블의 인덱스 자리에 맞게 1 표시. 나머지 0인 벡터
-# 배열로 만들기 때문에 2차원으로 변환
-# 두 가지 이상의 분류면 반드시 one-hot 인코딩!!!
+y = y-1
+print(y)
 
 from keras.utils import np_utils
 y = np_utils.to_categorical(y)
-# category 분류해준다
+# one-hot 인코딩을 하는 이유?
+# 현재의 y 값을 예시로 들어 2!=1*2, 3!=1*3,... 2배, 3배가 아니기 때문에 각 인덱스에 해당하는 자리로 표시해줘야 한다
+# 그래서 one-hot 인코딩을 하는 것이고 y의 종류에 따라 열이 결정됨
 
-# print(y)
-# print(y.shape)          # (10,6)
+print(y)
+print(y.shape)          # (10,6)
 # 우리가 의도하는 건 (10,5) 왜? print(y)의 0번째 열이 필요없다.
-y = y[:, 1:6]
+# y = y[:, 1:6]
 # print(y)
 # print(y.shape)          # (10,6)
 
@@ -42,9 +36,10 @@ model.add(Dense(90, activation='relu'))
 model.add(Dense(70, activation='relu'))
 model.add(Dense(50))
 model.add(Dense(5, activation='relu'))
+# softmax 거치기 전의 output==0.123 0.234 0.34 .. / 모두 더하면 1
 model.add(Dense(5, activation='softmax'))
 # 다중분류의 활성화함수는 softmax뿐
-
+# softmax를 거친 후의 output은 [0 0 1 0 0] 이런 식으로 인덱스 자리에 표시
 # model.summary()
 
 #3. 컴파일, 훈련

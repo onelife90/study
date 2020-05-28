d7b96800a0c78cadf8c37c52f64aeb0c06b08f2c
jieun-kim-debug <64455878+jieun-kim-debug@users.noreply.github.com>
2020-05-28 12:21:58 +0900

    Create keras58_mnist_lstm3.py

---
+++ keras/keras58_mnist_lstm3.py
@@ -0,0 +1,36 @@
+import numpy as np
+from keras.datasets import mnist
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
+
+#1. 데이터
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+#1-1. 데이터 전처리
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+print(y_train.shape)        # (60000, 10)
+print(y_test.shape)         # (10000, 10)
+
+x_train = x_train.reshape(x_train.shape[0], 392, 2).astype('float32')/255
+x_test = x_test.reshape(x_test.shape[0], 392, 2).astype('float32')/255
+
+print(x_train.shape)        # (60000, 392, 2)
+print(x_test.shape)         # (10000, 392, 2)
+
+#2. 모델 구성
+model = Sequential()
+model.add(LSTM(10, input_shape=(392,2)))
+model.add(Dense(10, activation='softmax'))
+
+#3. 컴파일, 훈련
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+model.fit(x_train, y_train, epochs=100, batch_size=600)
+
+#4. 평가, 예측
+loss, acc = model.evaluate(x_test, y_test, batch_size=600)
+
+print("loss: ", loss)
+print("acc: ", acc)

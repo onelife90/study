97dfa5ac3680f4f3578a50da5e24c5abdc1654fb
jieun-kim-debug <64455878+jieun-kim-debug@users.noreply.github.com>
2020-05-29 11:36:23 +0900

    Create keras69_cifar100_imshow.py

---
+++ keras/keras69_cifar100_imshow.py
@@ -0,0 +1,22 @@
+# cifar100 : 100가지 이미지를 찾는 데이터
+
+from keras.datasets import cifar100
+from keras.utils import np_utils
+from keras.models import Sequential, Model
+from keras.layers import Dense, LSTM, Conv2D, Flatten, MaxPooling2D, Dropout
+import matplotlib.pyplot as plt
+
+# 데이터 구조
+(x_train, y_train), (x_test, y_test) = cifar100.load_data()
+
+print("x_train[0]: \n", x_train[0])
+print("y_train[0]: \n", y_train)
+
+print(x_train.shape)            # (50000, 32, 32, 3)
+print(x_test.shape)             # (10000, 32, 32, 3)
+print(y_train.shape)            # (50000, 1)
+print(y_test.shape)             # (10000, 1)
+
+plt.imshow(x_train[0])
+plt.show()
+

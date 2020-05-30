e8536c2b2c266eaa6a1d019b328867213ff97dec
jieun-kim-debug <64455878+jieun-kim-debug@users.noreply.github.com>
2020-05-30 15:30:52 +0900

    Create keras82_breastCancer_dnn.py

---
+++ keras/keras82_breastCancer_dnn.py
@@ -0,0 +1,74 @@
+import numpy as np
+from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+import matplotlib.pyplot as plt
+
+#1. 데이터
+breast_cancer = load_breast_cancer()
+x = breast_cancer.data
+y = breast_cancer.target
+
+# print(x[0])         # 30개 컬럼
+# print(y[0])         # 0,1 이진분류
+# print(x.shape)      # (569,30)
+# print(y.shape)      # (569, )
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=77, train_size=0.6)
+
+#2. 모델 구성
+model = Sequential()
+model.add(Dense(100, input_shape=(30, )))
+model.add(Dense(5000, activation='sigmoid'))
+model.add(Dense(9000, activation='sigmoid'))
+model.add(Dropout(0.1))
+model.add(Dense(7000, activation='sigmoid'))
+model.add(Dense(300, activation='sigmoid'))
+model.add(Dense(100, activation='sigmoid'))
+model.add(Dense(1, activation='sigmoid'))
+
+#3. 컴파일, 훈련
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+early_stop = EarlyStopping(monitor='loss', patience=5, mode='auto')
+modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
+checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True)
+tb_hist = TensorBoard(log_dir='graph', histogram_freq=0,  write_graph=True, write_images=True)
+hist = model.fit(x_train, y_train, epochs=500, batch_size=5, validation_split=0.2, callbacks=[early_stop, checkpoint, tb_hist])
+
+#4. 평가, 예측
+loss, acc = model.evaluate(x_test, y_test, batch_size=5)
+
+print("loss: ", loss)
+print("acc: ", acc)
+
+loss = hist.history['loss']
+acc = hist.history['acc']
+val_loss = hist.history['val_loss']
+val_acc = hist.history['val_acc']
+
+#5. 시각화
+plt.figure(figsize=(10,6))
+plt.subplot(2,1,1)
+plt.plot(loss, marker='.', c='red', label='loss')
+plt.plot(val_loss, marker='.', c='blue', label='val_loss')
+plt.title('loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend()
+
+plt.subplot(2,1,2)
+plt.plot(acc, marker='.', c='red', label='acc')
+plt.plot(val_acc, marker='.', c='blue', label='val_acc')
+plt.title('acc')
+plt.ylabel('acc')
+plt.xlabel('epoch')
+plt.legend()
+plt.show()
+
+# 튜닝
+# epochs=25,batch=5,노드=100,5000,9000,drop0.1,7000,300,100
+#loss:  0.660052951229246
+#acc:  0.6315789222717285

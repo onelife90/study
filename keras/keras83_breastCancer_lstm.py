db7fad827bc929a47f4148ea19371f710246ea91
jieun-kim-debug <onelife90@naver.com>
2020-05-30 16:25:23 +0900

    Update keras83_breastCancer_lstm.py

--- keras/keras83_breastCancer_lstm.py
+++ keras/keras83_breastCancer_lstm.py
@@ -25,16 +25,16 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=77, train
 #2. 모델 구성
 model = Sequential()
 model.add(LSTM(50, input_shape=(6,5)))
-model.add(Dense(5000, activation='sigmoid'))
-model.add(Dense(9000, activation='sigmoid'))
+model.add(Dense(9000))
+model.add(Dense(5000))
 model.add(Dropout(0.1))
-model.add(Dense(7000, activation='sigmoid'))
-model.add(Dense(300, activation='sigmoid'))
-model.add(Dense(100, activation='sigmoid'))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(7000))
+model.add(Dense(300))
+model.add(Dense(100))
+model.add(Dense(1))
 
 #3. 컴파일, 훈련
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+model.compile(loss='mse', optimizer='adam', metrics=['mse'])
 early_stop = EarlyStopping(monitor='loss', patience=5, mode='auto')
 modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
 checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True)
@@ -42,15 +42,24 @@ tb_hist = TensorBoard(log_dir='graph', histogram_freq=0,  write_graph=True, writ
 hist = model.fit(x_train, y_train, epochs=500, batch_size=5, validation_split=0.2, callbacks=[early_stop, checkpoint, tb_hist])
 
 #4. 평가, 예측
-loss, acc = model.evaluate(x_test, y_test, batch_size=5)
+loss, mse = model.evaluate(x_test, y_test, batch_size=5)
+y_predict = model.predict(x_test)
 
-print("loss: ", loss)
-print("acc: ", acc)
+# RMSE 구하기
+from sklearn.metrics import mean_squared_error
+def RMSE(y_test, y_predict):
+    return np.sqrt(mean_squared_error(y_test, y_predict))
+print("RMSE: ", RMSE)
+
+# R2 구하기
+from sklearn.metrics import r2_score
+r2 = r2_score(y_test, y_predict)
+print("r2: ", r2)
 
 loss = hist.history['loss']
-acc = hist.history['acc']
+mse = hist.history['mse']
 val_loss = hist.history['val_loss']
-val_acc = hist.history['val_acc']
+val_mse = hist.history['val_mse']
 
 #5. 시각화
 plt.figure(figsize=(10,6))
@@ -63,15 +72,10 @@ plt.xlabel('epoch')
 plt.legend()
 
 plt.subplot(2,1,2)
-plt.plot(acc, marker='.', c='red', label='acc')
-plt.plot(val_acc, marker='.', c='blue', label='val_acc')
-plt.title('acc')
-plt.ylabel('acc')
+plt.plot(mse, marker='.', c='red', label='acc')
+plt.plot(val_mse, marker='.', c='blue', label='val_mse')
+plt.title('mse')
+plt.ylabel('mse')
 plt.xlabel('epoch')
 plt.legend()
 plt.show()
-
-# 튜닝
-# epochs=25,batch=5,노드=100,5000,9000,drop0.1,7000,300,100
-#loss:  0.660052951229246
-#acc:  0.6315789222717285

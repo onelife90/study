cee5685495f9c3252bb3b2fd0375004d470eb0e9
jieun-kim-debug <onelife90@naver.com>
2020-05-31 15:40:09 +0900

    Update keras82_breastCancer_dnn.py

--- keras/keras82_breastCancer_dnn.py
+++ keras/keras82_breastCancer_dnn.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.datasets import load_breast_cancer
+from keras.utils import np_utils
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
@@ -14,11 +15,13 @@ x = breast_cancer.data
 y = breast_cancer.target
 
 # print(x[0])         # 30개 컬럼
-# print(y)            # 0,1 이진분류
+# print(y)            # 0,1로 여러개
 # print(x.shape)      # (569,30)
 # print(y.shape)      # (569, )
 
 #1-1. 데이터 전처리
+y = np_utils.to_categorical(y)
+# print(y.shape)      # (569,2)
 scaler = StandardScaler()
 x = scaler.fit_transform(x)
 # print(x.shape)      # (569,30)
@@ -28,16 +31,16 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=77, train
 #2. 모델 구성
 model = Sequential()
 model.add(Dense(100, input_shape=(30, )))
-model.add(Dense(5000, activation='sigmoid'))
-model.add(Dense(9000, activation='sigmoid'))
+model.add(Dense(5000, activation='softmax'))
+model.add(Dense(9000, activation='softmax'))
 model.add(Dropout(0.1))
-model.add(Dense(7000, activation='sigmoid'))
-model.add(Dense(300, activation='sigmoid'))
-model.add(Dense(100, activation='sigmoid'))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(7000, activation='softmax'))
+model.add(Dense(300, activation='softmax'))
+model.add(Dense(100, activation='softmax'))
+model.add(Dense(2, activation='softmax'))
 
 #3. 컴파일, 훈련
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 early_stop = EarlyStopping(monitor='loss', patience=5, mode='auto')
 modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
 checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True)

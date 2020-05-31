b4dbb75de340ce1bd319c2abb763d91da41e0718
jieun-kim-debug <onelife90@naver.com>
2020-05-31 11:19:07 +0900

    Update keras73_boston_dnn.py

--- keras/keras73_boston_dnn.py
+++ keras/keras73_boston_dnn.py
@@ -10,7 +10,7 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 import matplotlib.pyplot as plt
 
 #1. 데이터
-boston =  load_boston()
+boston = load_boston()
 x = boston.data
 y = boston.target
 # print(x.shape)        # (506, 13)
@@ -38,7 +38,7 @@ output1 = Dense(1)(dense1)
 model = Model(inputs=input1, outputs=output1)
 
 #3. 컴파일, 훈련
-model.compile(loss='mse', optimizer='adam', metrics=['acc'])
+model.compile(loss='mse', optimizer='adam', metrics=['mse'])
 earlystopping = EarlyStopping(monitor='mse', patience=5, mode='auto')
 modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
 checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_mse', save_best_only=True, mode='auto')
@@ -46,47 +46,43 @@ tb_hist = TensorBoard(log_dir='graph', histogram_freq=0, write_graph=True, write
 hist = model.fit(x_train, y_train, epochs=100, batch_size=1, validation_split=0.2, callbacks=[earlystopping, checkpoint, tb_hist])
 
 #4. 평가, 예측
-RMSE, r2 = model.evaluate(x_test, y_test, batch_size=1)
+loss, mse = model.evaluate(x_test, y_test, batch_size=1)
 y_predict = model.predict(x_test)
-print("y_predict: \n", y_predict)
+# print("y_predict: \n", y_predict)
 
 # RMSE 구하기
 from sklearn.metrics import mean_squared_error
 def RMSE(y_test, y_predict):
     return np.sqrt(mean_squared_error(y_test, y_predict))
-print("RMSE: \n", RMSE)
+print("RMSE: ", RMSE)
 
 # R2 구하기
 from sklearn.metrics import r2_score
 r2 = r2_score(y_test, y_predict)
-print("r2: \n", r2)
-'''
-RMSE = hist.history['RMSE']
-r2 = hist.history['r2']
-val_RMSE = hist.history['val_RMSE']
-val_r2 = hist.history['val_r2']
+print("r2: ", r2)
 
-print("val_RMSE: \n", val_RMSE)
-print("val_r2: \n", val_r2)
+loss = hist.history['loss']
+mse = hist.history['mse']
+val_loss = hist.history['val_loss']
+val_mse = hist.history['val_mse']
 
 #5. 시각화
 plt.figure(figsize=(10,6))
 plt.subplot(2,1,1)
-plt.plot(RMSE, marker='.', c='red', label='RMSE')
-plt.plot(val_RMSE, marker='.', c='blue', label='val_RMSE')
+plt.plot(loss, marker='.', c='red', label='loss')
+plt.plot(val_loss, marker='.', c='blue', label='val_loss')
 plt.grid()
-plt.title('RMSE')
-plt.ylabel('RMSE')
+plt.title('loss')
+plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend()
 
 plt.subplot(2,1,2)
-plt.plot(r2, marker='.', c='red', label='r2')
-plt.plot(val_r2, marker='.', c='blue', label='val_r2')
+plt.plot(mse, marker='.', c='red', label='mse')
+plt.plot(val_mse, marker='.', c='blue', label='val_mse')
 plt.grid()
-plt.title('r2')
-plt.ylabel('r2')
+plt.title('mse')
+plt.ylabel('mse')
 plt.xlabel('epoch')
 plt.legend()
 plt.show()
-'''

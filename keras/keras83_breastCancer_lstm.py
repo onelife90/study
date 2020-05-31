c580f88736f3cf7f7491412820eecd9f2f74c728
jieun-kim-debug <onelife90@naver.com>
2020-05-31 15:48:39 +0900

    Update keras83_breastCancer_lstm.py

--- keras/keras83_breastCancer_lstm.py
+++ keras/keras83_breastCancer_lstm.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.datasets import load_breast_cancer
+from keras.utils import np_utils
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
@@ -13,28 +14,30 @@ x = breast_cancer.data
 y = breast_cancer.target
 
 # print(x[0])         # 30개 컬럼
-# print(y[0])         # 0,1 이진분류
+# print(y[0])         # 0,1 여러개. 다중분류
 # print(x.shape)      # (569,30)
 # print(y.shape)      # (569, )
 
+y = np_utils.to_categorical(y)
+# print(y.shape)      # (569,2)
 x = x.reshape(-1,6,5)
-print(x.shape)      # (596,6,5)
+# print(x.shape)      # (596,6,5)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=77, train_size=0.6)
 
 #2. 모델 구성
 model = Sequential()
 model.add(LSTM(50, input_shape=(6,5)))
-model.add(Dense(9000, activation='sigmoid'))
-model.add(Dense(5000, activation='sigmoid'))
+model.add(Dense(9000, activation='softmax'))
+model.add(Dense(5000, activation='softmax'))
 model.add(Dropout(0.1))
-model.add(Dense(7000, activation='sigmoid'))
-model.add(Dense(300, activation='sigmoid'))
-model.add(Dense(100, activation='sigmoid'))
-model.add(Dense(1))
+model.add(Dense(7000, activation='softmax'))
+model.add(Dense(300, activation='softmax'))
+model.add(Dense(100, activation='softmax'))
+model.add(Dense(2, activation='softmax'))
 
 #3. 컴파일, 훈련
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 early_stop = EarlyStopping(monitor='loss', patience=5, mode='auto')
 modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
 checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True)
@@ -71,3 +74,8 @@ plt.ylabel('acc')
 plt.xlabel('epoch')
 plt.legend()
 plt.show()
+
+# 튜닝
+# epochs=23,batch=5,노드=LSTM50,9000,5000,drop0.1,7000,300,100
+#loss:  0.6581311159228024
+#acc:  0.6315789222717285

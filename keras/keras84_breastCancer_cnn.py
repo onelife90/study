9fc3509feaa871c1fc70483784657443b6121093
jieun-kim-debug <onelife90@naver.com>
2020-05-31 14:49:42 +0900

    Update keras84_breastCancer_cnn.py

--- keras/keras84_breastCancer_cnn.py
+++ keras/keras84_breastCancer_cnn.py
@@ -14,7 +14,7 @@ x = breast_cancer.data
 y = breast_cancer.target
 
 # print(x[0])         # 30개 컬럼
-# print(y[0])         # 예측..모델 회귀모델
+# print(y)            # 0,1 이진분류
 # print(x.shape)      # (569,30)
 # print(y.shape)      # (569, )
 
@@ -26,42 +26,36 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=77, train
 #2. 모델 구성
 model = Sequential()
 model.add(Conv2D(5000, (2,2), padding='same', input_shape=(6,5,1)))
-model.add(Conv2D(3000, (2,2), padding='same'))
-model.add(Conv2D(9000, (2,2), padding='same'))
-model.add(MaxPooling2D(pool_size=2, padding='same'))
+model.add(Conv2D(3000, (2,2), padding='same', activation='sigmoid'))
+model.add(Conv2D(9000, (2,2), padding='same', activation='sigmoid'))
+model.add(MaxPooling2D(pool_size=2, padding='same', activation='sigmoid'))
 model.add(Dropout(0.1))
-model.add(Conv2D(400, (2,2), padding='same'))
-model.add(Conv2D(200, (2,2), padding='same'))
-model.add(MaxPooling2D(pool_size=2, padding='same'))
-model.add(Conv2D(10, (2,2), padding='same'))
+model.add(Conv2D(400, (2,2), padding='same', activation='sigmoid'))
+model.add(Conv2D(200, (2,2), padding='same', activation='sigmoid'))
+model.add(MaxPooling2D(pool_size=2, padding='same', activation='sigmoid'))
+model.add(Conv2D(10, (2,2), padding='same', activation='sigmoid'))
 model.add(Flatten())
-model.add(Dense(1))
+model.add(Dense(1, activation='sigmoid'))
 
 #3. 컴파일, 훈련
-model.compile(loss='mse', optimizer='adam', metrics=['mse'])
+model.compile(loss='binary_srossentropy', optimizer='adam', metrics=['acc'])
 early_stop = EarlyStopping(monitor='loss', patience=5, mode='auto')
 modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
 checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True)
 tb_hist = TensorBoard(log_dir='graph', histogram_freq=0,  write_graph=True, write_images=True)
-hist = model.fit(x_train, y_train, epochs=500, batch_size=5, validation_split=0.2, callbacks=[early_stop, checkpoint, tb_hist])
+hist = model.fit(x_train, y_train, epochs=500, batch_size=1, validation_split=0.2, callbacks=[early_stop, checkpoint, tb_hist])
 
 #4. 평가, 예측
-loss, mse = model.evaluate(x_test, y_test, batch_size=5)
+loss, acc = model.evaluate(x_test, y_test, batch_size=1)
 y_predict = model.predict(x_test)
 
-#RMSE 구하기
-def RMSE(y_test, y_predict):
-    return np.sqrt(mean_squared_error(y_test, y_predict))
-print("RMSE: ", RMSE)
-
-#R2 구하기
-r2 = r2_score(y_test, y_predict)
-print("R2: ", r2)
+print("loss: ", loss)
+print("acc: ", acc)
 
 loss = hist.history['loss']
-mse = hist.history['mse']
+acc = hist.history['acc']
 val_loss = hist.history['val_loss']
-val_mse = hist.history['val_mse']
+val_acc = hist.history['val_acc']
 
 #5. 시각화
 plt.figure(figsize=(10,6))
@@ -74,14 +68,10 @@ plt.xlabel('epoch')
 plt.legend()
 
 plt.subplot(2,1,2)
-plt.plot(mse, marker='.', c='red', label='mse')
-plt.plot(val_mse, marker='.', c='blue', label='val_mse')
-plt.title('mse')
-plt.ylabel('mse')
+plt.plot(acc, marker='.', c='red', label='acc')
+plt.plot(val_acc, marker='.', c='blue', label='val_acc')
+plt.title('acc')
+plt.ylabel('acc')
 plt.xlabel('epoch')
 plt.legend()
 plt.show()
-
-# 튜닝
-# epochs=29,batch=5,노드=5000,3000,9000,max2,drop0.1,400,200,max2,10,flat
-

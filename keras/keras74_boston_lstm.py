b5a10a2150cdb7ab8bbc01f73e78b9b3656e09e0
jieun-kim-debug <onelife90@naver.com>
2020-05-31 11:27:06 +0900

    Update keras74_boston_lstm.py

--- keras/keras74_boston_lstm.py
+++ keras/keras74_boston_lstm.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from keras.models import Sequential, Model, Input
 from keras.layers import Dense, Dropout, LSTM
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 
 #1. 데이터
@@ -23,7 +24,7 @@ y = y.reshape(y.shape[0],1)
 scaler = MinMaxScaler()
 x = scaler.fit_transform(x)
 # print(x[1])
-x = x.reshape(x.shape[0], x.shape[1], 1)
+x = x.reshape(-1, x.shape[1], 1)
 # print(x.shape)                  # (506, 13, 1)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.6)
@@ -35,7 +36,7 @@ print(y_test.shape)         # (203, 1)
 #2. 모델 구성
 input1 = Input(shape=(13,1))
 dense1 = LSTM(10, return_sequences=True)(input1)
-dense1 = LSTM(1)(input1)
+dense1 = LSTM(1)(dense1)
 dense1 = Dense(5000)(dense1)
 dense1 = Dropout(0.2)(dense1)
 dense1 = Dense(500)(dense1)
@@ -48,57 +49,44 @@ model = Model(inputs=input1, outputs=output1)
 model.compile(loss='mse', optimizer='adam', metrics=['mse'])
 earlystopping = EarlyStopping(monitor='mse', patience=5, mode='auto')
 modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
-checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_mse', save_best_only=True, mode='auto')
+checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True, mode='auto')
 tb_hist = TensorBoard(log_dir='graph', histogram_freq=0, write_graph=True, write_images=True, )
 hist = model.fit(x_train, y_train, epochs=100, batch_size=1, validation_split=0.2, callbacks=[earlystopping, checkpoint, tb_hist])
 
 #4. 평가, 예측
-RMSE, r2 = model.evaluate(x_test, y_test, batch_size=1)
-y_predict = model.predict(x_test)
-# print("y_predict: \n", y_predict)
+loss, mse = model.evaluate(x_test, y_test, batch_size=1)
+y_pred = model.predict(x_test)
 
-# RMSE 구하기
-from sklearn.metrics import mean_squared_error
-def RMSE(y_test, y_predict):
-    return np.sqrt(mean_squared_error(y_test, y_predict))
-print("RMSE: \n", RMSE)
+#RMSE 구하기
+def RMSE(y_test, y_pred):
+    return np.sqrt(mean_squared_error(y_test, y_pred))
+print("RMSE: ", RMSE(y_test, y_pred))
 
-# R2 구하기
-from sklearn.metrics import r2_score
-r2 = r2_score(y_test, y_predict)
-print("r2: \n", r2)
-'''
-RMSE = hist.history['RMSE']
-r2 = hist.history['r2']
-val_RMSE = hist.history['val_RMSE']
-val_r2 = hist.history['val_r2']
+#R2 구하기
+r2 = r2_score(y_test, y_pred)
+print("R2: ", r2)
 
-print("val_RMSE: \n", val_RMSE)
-print("val_r2: \n", val_r2)
+loss = hist.history['loss']
+mse = hist.history['mse']
+val_loss = hist.history['val_loss']
+val_mse = hist.history['val_mse']
 
 #5. 시각화
 plt.figure(figsize=(10,6))
 plt.subplot(2,1,1)
-plt.plot(RMSE, marker='.', c='red', label='RMSE')
-plt.plot(val_RMSE, marker='.', c='blue', label='val_RMSE')
-plt.grid()
-plt.title('RMSE')
-plt.ylabel('RMSE')
+plt.plot(loss, marker='.', c='red', label='loss')
+plt.plot(val_loss, marker='.', c='blue', label='val_loss')
+plt.title('loss')
+plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend()
 
 plt.subplot(2,1,2)
-plt.plot(r2, marker='.', c='red', label='r2')
-plt.plot(val_r2, marker='.', c='blue', label='val_r2')
-plt.grid()
-plt.title('r2')
-plt.ylabel('r2')
+plt.plot(mse, marker='.', c='red', label='mse')
+plt.plot(val_mse, marker='.', c='blue', label='val_mse')
+plt.title('mse')
+plt.ylabel('mse')
 plt.xlabel('epoch')
 plt.legend()
-plt.show()
-'''
 
-# 튜닝
-# epochs=31,노드=169,13,drop0.2.max4,drop0.1,303
-#RMSE:  <function RMSE at 0x00000193E7678168>
-#r2:  0.46706543866600814
+plt.show()

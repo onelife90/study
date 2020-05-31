def034285391e90d70e9ad26e21f62665d0dd7b0
jieun-kim-debug <onelife90@naver.com>
2020-05-31 16:10:44 +0900

    Update keras84_breastCancer_cnn.py

--- keras/keras84_breastCancer_cnn.py
+++ keras/keras84_breastCancer_cnn.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.datasets import load_breast_cancer
+from keras.utils import np_utils
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
@@ -14,10 +15,12 @@ x = breast_cancer.data
 y = breast_cancer.target
 
 # print(x[0])         # 30개 컬럼
-# print(y)            # 0,1 이진분류
+# print(y)            # 0,1 여러개 다중분류
 # print(x.shape)      # (569,30)
 # print(y.shape)      # (569, )
 
+y = np_utils.to_categorical(y)
+# print(y.shape)      # (596,2)
 x = x.reshape(-1,6,5,1)
 # print(x.shape)      # (596,6,5,1)
 
@@ -26,19 +29,19 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=77, train
 #2. 모델 구성
 model = Sequential()
 model.add(Conv2D(5000, (2,2), padding='same', input_shape=(6,5,1)))
-model.add(Conv2D(3000, (2,2), padding='same', activation='sigmoid'))
-model.add(Conv2D(9000, (2,2), padding='same', activation='sigmoid'))
+model.add(Conv2D(3000, (2,2), padding='same', activation='softmax'))
+model.add(Conv2D(9000, (2,2), padding='same', activation='softmax'))
 model.add(MaxPooling2D(pool_size=2, padding='same'))
 model.add(Dropout(0.1))
-model.add(Conv2D(400, (2,2), padding='same', activation='sigmoid'))
-model.add(Conv2D(200, (2,2), padding='same', activation='sigmoid'))
+model.add(Conv2D(400, (2,2), padding='same', activation='softmax'))
+model.add(Conv2D(200, (2,2), padding='same', activation='softmax'))
 model.add(MaxPooling2D(pool_size=2, padding='same'))
-model.add(Conv2D(10, (2,2), padding='same', activation='sigmoid'))
+model.add(Conv2D(10, (2,2), padding='same', activation='softmax'))
 model.add(Flatten())
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(2, activation='softmax'))
 
 #3. 컴파일, 훈련
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 early_stop = EarlyStopping(monitor='loss', patience=5, mode='auto')
 modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
 checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True)
@@ -78,5 +81,5 @@ plt.show()
 
 # 튜닝
 # epochs=12,batch=1,노드=5000,3000,9000,max2,drop0.1,400,200,max2,10,flat
-#loss:  0.6592508344012394
+#loss:  0.6581239072900069
 #acc:  0.6315789222717285

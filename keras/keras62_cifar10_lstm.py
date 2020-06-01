a320ddded1441f47f565c55544b386a65a714249
jieun-kim-debug <onelife90@naver.com>
2020-06-01 20:24:58 +0900

    Update keras62_cifar10_lstm.py

--- keras/keras62_cifar10_lstm.py
+++ keras/keras62_cifar10_lstm.py
@@ -6,7 +6,7 @@ from keras.utils import np_utils
 from keras.models import Sequential, Model
 from keras.layers import Dense, LSTM, Conv2D, Flatten, MaxPooling2D, Dropout, Input
 import matplotlib.pyplot as plt
-from keras.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 
 #1. 데이터
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -25,27 +25,34 @@ y_test = np_utils.to_categorical(y_test)
 # print(y_train.shape)        # (50000, 10)
 # print(y_test.shape)         # (10000, 10)
 
-x_train = x_train.reshape(-1,32*32,3).astype('float32')/255
-x_test = x_test.reshape(-1,32*32,3).astype('float32')/255
-# print(x_train.shape)        # (50000, 1024, 3)
-# print(x_test.shape)         # (10000, 1024, 3)
+x_train = x_train.reshape(-1,8,32*3*4).astype('float32')/255
+x_test = x_test.reshape(-1,8,32*3*4).astype('float32')/255
+# print(x_train.shape)        # (50000, 8, 384)
+# print(x_test.shape)         # (10000, 8, 384)
 
 #2. 모델 구성
-input1 = Input(shape=(1024,3))
-dense1 = LSTM(5)(input1)
-dense1 = Dense(10)(dense1)
-dense1 = Dropout(0.3)(dense1)
+input1 = Input(shape=(8,384))
+dense1 = LSTM(50)(input1)
+dense1 = Dense(5000)(dense1)
+dense1 = Dense(4000)(dense1)
+dense1 = Dense(1000)(dense1)
+dense1 = Dropout(0.1)(dense1)
 output1 = Dense(10, activation='softmax')(dense1)
 
 model = Model(inputs=input1, outputs=output1)
 
 #3. 컴파일, 훈련
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
-earlystopping = EarlyStopping(monitor='loss', patience=100, mode='auto')
-model.fit(x_train, y_train, epochs=500, batch_size=500, validation_split=0.3, callbacks=[earlystopping])
+earlystopping = EarlyStopping(monitor='loss', patience=5, mode='auto')
+modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
+checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True, save_weights_only=False)
+model.fit(x_train, y_train, epochs=1000, batch_size=100, validation_split=0.2, callbacks=[earlystopping, checkpoint])
+
+model.save('./model/sample/cifar10/cifar10_checkpoint_best.h5')
+
 
 #4. 평가, 예측
-loss, acc = model.evaluate(x_test, y_test, batch_size=500)
+loss, acc = model.evaluate(x_test, y_test, batch_size=100)
 
 print("loss: ", loss)
 print("acc: ", acc)

712e2b990759d02eb21f9baf85bd810393be0e89
jieun-kim-debug <onelife90@naver.com>
2020-06-02 09:11:27 +0900

    Update keras65_fashion_dnn.py

--- keras/keras65_fashion_dnn.py
+++ keras/keras65_fashion_dnn.py
@@ -1,12 +1,12 @@
 # Sequential형으로 완성하시오
 # 하단에 주석으로 acc와 loss 결과 명시
-# LSTM=3차원 / 현재 x데이터(3차원)==> reshape 필요없음 ==> input_shape=2차원 
+# DNN=2차원 / 현재 x데이터(3차원)==> 2차원 reshape ==> input_shape=1차원 벡터
 
 import numpy as np
 from keras.datasets import fashion_mnist
 from keras.utils import np_utils
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, LSTM
+from keras.layers import Dense, Dropout
 from keras.callbacks import EarlyStopping
 
 #1. 데이터
@@ -23,23 +23,37 @@ y_test = np_utils.to_categorical(y_test)
 # print(y_train.shape)        # (60000, 10)
 # print(y_test.shape)         # (10000, 10)
 
+x_train = x_train.reshape(-1,28*28).astype('float32')/255
+x_test = x_test.reshape(-1,28*28).astype('float32')/255
+print(x_train.shape)        # (60000, 784)
+print(x_test.shape)         # (10000, 784)
+
 #2. 모델 구성
 model = Sequential()
-model.add(LSTM(10, input_shape=(28,28)))
+model.add(Dense(10, input_shape=(784, )))
 model.add(Dense(9000))
-model.add(Dropout(0.3))
-model.add(Dense(500))
-model.add(Dropout(0.3))
+model.add(Dropout(0.1))
+model.add(Dense(5000))
+model.add(Dense(3000))
+model.add(Dense(1000))
+model.add(Dropout(0.1))
 model.add(Dense(100))
 model.add(Dense(10, activation='softmax'))
 
 #3. 컴파일, 훈련
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
-earlystopping = EarlyStopping(monitor='loss', patience=300, mode='auto')
-model.fit(x_train, y_train, epochs=100, batch_size=300, validation_split=0.3, callbacks=[earlystopping])
+earlystopping = EarlyStopping(monitor='loss', patience=5, mode='auto')
+model.fit(x_train, y_train, epochs=1000, batch_size=100, validation_split=0.2, callbacks=[earlystopping])
+
+model.save_weights('./model/sample/fashion/fsahion_save_weights.h5')
 
 #4. 평가, 예측
-loss, acc = model.evaluate(x_test, y_test, batch_size=300)
+loss, acc = model.evaluate(x_test, y_test, batch_size=100)
 
 print("loss: ", loss)
 print("acc: ", acc)
+
+# 튜닝
+# epochs=15,batch=100,node=10,9000,drop0.1,5000,3000,1000,drop0.1,100
+# loss:  0.6607936990261077
+# acc:  0.8039000034332275

9a14ebaa8b2fabb35ad514194e543f6f24f6e8b6
jieun-kim-debug <onelife90@naver.com>
2020-06-03 20:46:02 +0900

    Update test0602_model1.py

--- keras/test0602_model1.py
+++ keras/test0602_model1.py
@@ -8,6 +8,8 @@ from keras.layers.merge import concatenate
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.decomposition import PCA
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+import matplotlib.pyplot as plt
 
 #2-1. 데이터 불러오기
 samsung = np.load('./data/samsung.npy', allow_pickle=True)
@@ -23,7 +25,7 @@ hite = np.load('./data/hite.npy', allow_pickle=True)
 
 #2-1-1. 데이터 벡터화(1컬럼짜리 데이터)
 samsung = samsung.reshape(samsung.shape[0], )
-print(samsung.shape)        # (509,)
+# print(samsung.shape)        # (509,)
 
 #2-1-2. 2차원으로 바꿔주는 split 함수
 size = 6
@@ -40,8 +42,8 @@ samsung = split_x(samsung, size)
 
 #2-1-3. samsung 데이터 x, y 자르기
 # 0:5 컬럼까지 x로 두고, 마지막 컬럼을 y로 쓰겠다
-x_sam = samsung[:, 0:5]
-y_sam = samsung[:, 5]
+x1 = samsung[:, 0:5]
+y = samsung[:, 5]
 # print(x_sam.shape)      # (504, 5)
 # print(y_sam.shape)      # (504,)
 
@@ -53,17 +55,29 @@ y_sam = samsung[:, 5]
 # 그럼 어떻게 자를까? hite의 가장 오래된 행을 잘라보는 편이 낫다
 
 #2-1-4. hite 데이터 행맞추기
-x_hite = hite[5:510, :] 
+x2 = hite[5:510, :] 
 # print(x_hite.shape)     # (504, 5)
+
+#2-1-5. train_test_split
+x1_train, x1_test, x2_train, x2_test, y_train, y_test = train_test_split(x1, x2, y, train_size=0.6, random_state=99)
+# 변수명_train, 변수명_test라고 명시해야 에러 X
+# print(x1_train.shape)       # (302, 5)
+# print(x1_test.shape)        # (202, 5)
+# print(x2_train.shape)       # (302, 5)
+# print(x2_test.shape)        # (202, 5)
 
 #2-2. 모델 구성
 input1 = Input(shape=(5, ))
-x1 = Dense(10)(input1)
-x1 = Dense(100)(x1)
+x1 = Dense(100)(input1)
+x1 = Dense(1000)(x1)
+x1 = Dense(3000)(x1)
+x1 = Dense(5000)(x1)
 
 input2 = Input(shape=(5, ))
 x2 = Dense(100)(input2)
-x2 = Dense(20)(x2)
+x2 = Dense(1000)(input2)
+x2 = Dense(3000)(input2)
+x2 = Dense(5000)(input2)
 
 merge = concatenate([x1, x2])
 
@@ -71,8 +85,56 @@ output = Dense(1)(merge)
 
 model = Model(inputs=[input1, input2], outputs=output)
 
-model.summary()
+# model.summary()
 
 #3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam', metrics=['mse'])
-model.fit([x_sam, x_hite], y_sam, epochs=5)
+early_stop = EarlyStopping(monitor='loss', patience=3, mode='auto')
+modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
+check_p = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True, save_weights_only=False)
+tb_hist = TensorBoard(log_dir='graph', histogram_freq=0, write_graph=True, write_images=True)
+hist = model.fit([x1_train, x2_train], y_train, epochs=100, batch_size=1, validation_split=0.2, callbacks=[early_stop, check_p, tb_hist])
+
+loss = hist.history['loss']
+val_loss = hist.history['val_loss']
+mse = hist.history['mse']
+val_mse = hist.history['val_mse']
+
+#4. 평가, 예측
+loss, mse = model.evaluate([x1_test, x2_test], y_test)
+
+# print(x1_test[-1, :])       # [49700 49000 48500 47600 47200] 벡터
+x1_pred = x1_test[-1, :].reshape(-1,5)
+print(x1_pred)              # [[49700 49000 48500 47600 47200]] 행렬
+# 벡터를 2차원으로 reshape ==> 행렬
+x2_pred = x2_test[-1, :].reshape(-1,5)
+print(x2_pred)              # [[19900 20150 19800 20100 254623]] 행렬
+# 벡터를 2차원으로 reshape ==> 행렬
+y_pred = model.predict([x1_pred, x2_pred])
+print("y_pred: \n", y_pred)
+
+'''
+print("loss: ", loss)
+print("mse: ", mse)
+print("y_pred: \n", y_pred)
+print(y_pred.shape)
+
+#5. 시각화
+plt.figure(figsize=(10,6))
+plt.subplot(2,1,1)
+plt.plot(loss, marker='.', c='red', label='loss')
+plt.plot(val_loss, marker='.', c='red', label='val_loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.title('loss')
+plt.legend()
+
+plt.subplot(2,1,2)
+plt.plot(mse, marker='.', c='red', label='mse')
+plt.plot(val_mse, marker='.', c='red', label='val_mse')
+plt.ylabel('mse')
+plt.xlabel('epoch')
+plt.title('mse')
+plt.legend()
+plt.show()
+'''

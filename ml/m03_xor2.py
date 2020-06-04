a8a56d12ed2dc855ba7a316baf74efb764982c3b
jieun-kim-debug <onelife90@naver.com>
2020-06-04 12:03:25 +0900

    Create m03_xor2.py

---
+++ ml/m03_xor2.py
@@ -0,0 +1,30 @@
+# xor 모델을 완성하시오.
+# SVC import
+
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+#1. 데이터
+x_data = [[0,0], [1,0], [0,1], [1,1]]
+y_data = [0,1,1,0]
+# 인풋 [0,0] 아웃풋 0
+# 인풋 [1,0] 아웃풋 1
+# 인풋 [0,1] 아웃풋 1
+# 인풋 [1,1] 아웃풋 0
+
+#2. 모델
+# model = LinearSVC(penalty='l1', loss='squared_hinge', dual=True)
+model = SVC()
+
+#3. 훈련
+model.fit(x_data, y_data)
+
+#4. 평가, 예측
+x_test = [[0,0], [1,0], [0,1], [1,1]]
+y_pred = model.predict(x_test)
+
+# score = model.evaluate(예측)
+acc = accuracy_score([0,1,1,0], y_pred)
+print(x_test, "의 예측 결과: ", y_pred)
+print("acc = ", acc)
+# acc =  1.0

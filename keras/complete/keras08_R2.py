0364458d7c85144a8594e1c93055e162acbf6609
jieun-kim-debug <onelife90@naver.com>
2020-06-05 19:24:30 +0900

    Update and rename keras/keras08_R2.py to keras/complete/keras08_R2.py

--- keras/complete/keras08_R2.py
+++ keras/complete/keras08_R2.py
@@ -10,14 +10,14 @@ x_train = np.array([1,2,3,4,5,6,7,8,9,10])
 y_train = np.array([1,2,3,4,5,6,7,8,9,10]) # 라인 복사 : ctrl+c / 라인 삭제 : shift+del
 x_test = np.array([11,12,13,14,15])
 y_test = np.array([11,12,13,14,15])
-x_pred = np.array([16,17,18]) # pred = predict
+x_pred = np.array([16,17,18]) 
 
 #2. 모델구성
 from keras.models import Sequential
 from keras.layers import Dense
 model = Sequential()
 
-model.add(Dense(5, input_dim = 1)) # activation에도 디폴트가 있다
+model.add(Dense(5, input_dim = 1))
 model.add(Dense(400))
 model.add(Dense(300))
 model.add(Dense(200))
@@ -37,8 +37,10 @@ model.add(Dense(20))
 model.add(Dense(1))
 
 #3. 훈련
-model.compile(loss='mse', optimizer='adam', metrics=['acc']) # metrics에는 대괄호 필수 문법임. 훈련 돌릴 때 보여지는 부분
-model.fit(x_train, y_train, epochs=80, batch_size=1) # w 가중치가 나옴
+model.compile(loss='mse', optimizer='adam', metrics=['mse']) 
+# metrics에는 대괄호 필수 문법임. 훈련 돌릴 때 보여지는 부분
+model.fit(x_train, y_train, epochs=80, batch_size=1) 
+# w 가중치가 나옴
 
 #4. 평가, 예측
 loss, mse = model.evaluate(x_test, y_test)
@@ -47,11 +49,11 @@ loss, mse = model.evaluate(x_test, y_test)
 
 print("loss : ", loss)
 print("mse : ", mse)
-
 # y_pred = model.predict(x_pred) #y_pred로 반환한다
 # print("y_predict : \n", y_pred)
 
-y_predict = model.predict(x_test) # 입력값에 y_test는 안되나요? ★절대안됨! x = input, y = output 이기 때문에!
+y_predict = model.predict(x_test) 
+# 입력값에 y_test는 안되나요? ★절대안됨! x = input, y = output 이기 때문에!
 print(y_predict)
 
 # RMSE 구하기
@@ -62,12 +64,13 @@ from sklearn.metrics import mean_squared_error
 def RMSE(y_test, y_predict): # def = 함수를 호출하겠다 () = 입력값
     return np.sqrt(mean_squared_error(y_test, y_predict)) # 반환 = 출력값
             #sqrt = 루트
-            #mse=(실제값-예측값)^2 / n 한 후 루트이기에 y_test, y_predict가 입력값이 됨
+            #mse=(실제값-예측값)^2의 합/n 한 후 루트이기에 y_test(실제값), y_predict(예측값)가 입력값이 됨
 print("RMSE : ", RMSE(y_test, y_predict))
 
 #R2 구하기
 from sklearn.metrics import r2_score
-r2 = r2_score(y_test, y_predict) #RMSE와 같이 비교하는 것이기에 r2_score에 입력값 같게
+r2 = r2_score(y_test, y_predict) 
+# RMSE와 같이 비교하는 것이기에 r2_score에 y_test(실제값), y_predict(예측값)가 입력값
 print("R2 : ", r2)
 
 # R2 = 1에 가까울수록 좋은 값. accuracy와 유사한 결과를 도출

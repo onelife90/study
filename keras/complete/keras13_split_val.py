ffb98a866b2a7abd7872a63feacc8aede1679d73
jieun-kim-debug <onelife90@naver.com>
2020-06-05 19:36:19 +0900

    Update and rename keras/keras13_split.py to keras/complete/keras13_split_val.py

--- keras/complete/keras13_split_val.py
+++ keras/complete/keras13_split_val.py
@@ -18,12 +18,7 @@ from sklearn.model_selection import train_test_split
 # train set과 test set을 손쉽게 분리할 수 있음
 # 하지만 과적합의 우려가 있기 때문에 validation set을 추가하여 방지
 # Train+Validation 훈련하면서 검증, TV로 생각하자
-x_train, x_test, y_train, y_test = train_test_split(
-    # x,y, random_state=99, shuffle=True,
-    x,y,
-    train_size=0.6
-)       
-
+x_train, x_test, y_train, y_test = train_test_split(x,y, train_size=0.6)       
 print(x_train)
 # print(x_val)
 print(x_test)
@@ -45,14 +40,12 @@ model.add(Dense(1))
 
 #3. 훈련
 model.compile(loss='mse', optimizer='adam', metrics=['mse']) 
-model.fit(x_train, y_train, epochs=300, batch_size=1,
-            validation_split=0.3)
-            # validation_split=0.3 / train_size=60 / 60% X 30% = 18%
-            # 즉, train=42%, val=18%의 비율로 훈련됨
+model.fit(x_train, y_train, epochs=300, batch_size=1,validation_split=0.3)
+# validation_split=0.3 / train_size=60 / 60% X 30% = 18%
+# 즉, train=42%, val=18%의 비율로 훈련됨
            
 #4. 평가, 예측
 loss, mse = model.evaluate(x_test, y_test)
-
 print("loss : ", loss)
 print("mse : ", mse)
 
@@ -61,10 +54,8 @@ print(y_predict)
 
 # RMSE 구하기
 from sklearn.metrics import mean_squared_error
-
 def RMSE(y_test, y_predict): 
     return np.sqrt(mean_squared_error(y_test, y_predict)) 
-            
 print("RMSE : ", RMSE(y_test, y_predict))
 
 #R2 구하기

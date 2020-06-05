424dbd69226e473318501fe64210f17a9c8a2c26
jieun-kim-debug <onelife90@naver.com>
2020-06-05 12:10:26 +0900

    Create m08_wine3.py

---
+++ ml/m08_wine3.py
@@ -0,0 +1,27 @@
+# wine의 y값이 편중되어 있는 문제!
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 와인 데이터 읽기
+wine = pd.read_csv('./data/winequality-white.csv', sep=';', header=0, index_col=None)
+
+# 데이터 안의 계수를 그룹화
+count_data = wine.groupby('quality')['quality'].count()
+# 숫자가 몇개인지 묶어서 count_data라는 변수명에 대입
+print(count_data)
+# quality
+# 3      20
+# 4     163
+# 5    1457
+# 6    2198
+# 7     880
+# 8     175
+# 9       5
+# Name: quality, dtype: int64
+# 머신도 5와 6으로 분류를 하기 때문에 낮은 acc
+
+count_data.plot()
+plt.show()
+
+# 이 문제를 어떻게 해결할까?

699a8a9363c9034826e9578afb6122cd917cebed
jieun-kim-debug <onelife90@naver.com>
2020-06-05 19:55:04 +0900

    Update and rename keras/keras21_ensemble_test.py to keras/complete/keras21_ensemble_test.py

--- keras/complete/keras21_ensemble_test.py
+++ keras/complete/keras21_ensemble_test.py
@@ -1,3 +1,5 @@
+# 앙상블 : 음악 합주 / 어마한 데이터 셋이 2개 이상을 합쳐보자
+
 #1. 데이터
 import numpy as np
 x1 = np.array([range(1,101), range(311,411), range(100)]) # 100개짜리 3덩어리
@@ -9,18 +11,18 @@ x1 = np.transpose(x1)
 y1 = np.transpose(y1)
 x2 = np.transpose(x2)
 y2 = np.transpose(y2)
-
-print(x1.shape)
-print(y1.shape)
-print(x2.shape)
-print(y2.shape)
+# print(x1.shape)     # (100,3)
+# print(y1.shape)     # (100,3)
+# print(x2.shape)     # (100,3)
+# print(y2.shape)     # (100,3)
 
 from sklearn.model_selection import train_test_split
 x1_train, x1_test, x2_train, x2_test, y1_train, y1_test, y2_train, y2_test = train_test_split(x1,x2,y1,y2, train_size=0.8)
 
 #2. 모델구성
 from keras.models import Sequential, Model
-from keras.layers import Dense, Input # 함수형 모델은 input, output을 명시해줘야함
+from keras.layers import Dense, Input 
+# 함수형 모델은 input, output을 명시해줘야함
 
 input1 = Input(shape=(3, ))
 dense1_1 = Dense(8000, activation='relu', name='bitking1')(input1)
@@ -35,6 +37,7 @@ from keras.layers.merge import concatenate
 # concatenate=사슬 같이 잇다 / 단순병합 : w가 반반으로 붙는다
 merge1 = concatenate([dense1_2, dense2_2])
 # merge1 레이어를 만들어줌 (M1_끝 레이어 + M2_끝 레이어)
+# 리스트 형태로 묶어줌
 middle1 = Dense(30)(merge1)
 middle1 = Dense(5)(middle1)
 middle1 = Dense(7)(middle1)
@@ -42,12 +45,13 @@ middle1 = Dense(7)(middle1)
 # output이 y1, y2이므로 아웃풋이 총 2개로 도출되어야 함
 
 ##### output 모델 구성 #####
-
-output1 = Dense(30)(middle1) # M1의 가장 끝 레이어가 middle1
+output1 = Dense(30)(middle1) 
+# M1의 가장 끝 레이어가 middle1
 output1_2 = Dense(7)(output1)
 output1_3 = Dense(3)(output1_2)
 
-output2 = Dense(30)(middle1) # M2의 가장 끝 레이어가 middle1
+output2 = Dense(30)(middle1) 
+# M2의 가장 끝 레이어가 middle1
 output2_2 = Dense(7)(output2)
 output2_3 = Dense(3)(output2_2)
 
@@ -60,14 +64,11 @@ model.summary()
 
 #3. 훈련
 model.compile(loss='mse', optimizer='adam', metrics=['mse']) 
-model.fit([x1_train, x2_train],
-          [y1_train, y2_train], epochs=1, batch_size=1,
-          validation_split=0.3, verbose=1)
-          # list로 묶어서 한번에 model.fit 완성
+model.fit([x1_train, x2_train], [y1_train, y2_train], epochs=1, batch_size=1, validation_split=0.3, verbose=1)
+# list로 묶어서 한번에 model.fit 완성
                    
 #4. 평가, 예측
-loss = model.evaluate([x1_test, x2_test],
-                           [y1_test, y2_test], batch_size=1)
+loss = model.evaluate([x1_test, x2_test], [y1_test, y2_test], batch_size=1)
 # 전체 loss 값(1)
 # y1_output1에 대한 loss(1)
 # y2_output2에 대한 loss(1)

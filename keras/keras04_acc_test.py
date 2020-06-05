d9733984f18b9a350af60364c278ec3e67aaaba6
jieun-kim-debug <onelife90@naver.com>
2020-06-05 19:09:47 +0900

    Update keras04_acc_test.py

--- keras/keras04_acc_test.py
+++ keras/keras04_acc_test.py
@@ -24,15 +24,18 @@ model.add(Dense(500))
 model.add(Dense(1))
 
 #3. 훈련
-model.compile(loss='mse', optimizer='adam', metrics=['acc']) #metrics에는 대괄호 필수 문법임. 훈련 돌릴 때 보여지는 부분
+model.compile(loss='mse', optimizer='adam', metrics=['acc'])
+# metrics에는 대괄호 필수 문법임. 훈련 돌릴 때 보여지는 부분
 model.fit(x, y, epochs=30, batch_size=1)
 
 #4. 평가, 예측
-loss, acc = model.evaluate(x, y) #평가 반환 값을 loss, acc(변수)에 넣겠다 #metrics<acc<evaluate
+loss, acc = model.evaluate(x, y) 
+# 평가 반환 값을 loss, acc(변수)에 넣겠다 #metrics<acc<evaluate
 print("loss : ", loss)
 print("acc : ", acc)
 
-y_pred = model.predict(x_pred) #y_pred로 반환한다
+y_pred = model.predict(x_pred) 
+# y_pred로 반환한다 y_pred라는 변수명에 대입해주는 것일뿐! 헷갈리지 말자
 print("y_predict : \n", y_pred)
 
 #y_predict : 완벽한 11,12,13 값이 아니다
@@ -43,4 +46,4 @@ print("y_predict : \n", y_pred)
 # #why? 회귀와 분류방식
 # 1. 회귀 : 수치를 넣었을 때 수치로 답을 한다
 # 2. 분류 : 강아지/고양이 _0과 1_ y값이 정해져있어야 함 ex) 동전을 던지고 난 결과
-#loss=mse, metrics=acc 이기 때문에 잘못된 모델을 쓴것임
+# loss=mse, metrics=acc 이기 때문에 잘못된 모델을 쓴것임

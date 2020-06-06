1f08ccee07f356540e2425bb1003b8b618a9e830
jieun-kim-debug <onelife90@naver.com>
2020-06-06 11:20:20 +0900

    Create p117_list_index.py

---
+++ homework/딥러닝교과서/p117_list_index.py
@@ -0,0 +1,9 @@
+# 리스트에서 값 추출
+# 리스트 안 오브젝트에는 번호가 할당 0부터
+# 고것을 인덱스 번호
+
+fruits = ["apple", 2, "orange",4,"grape", 3, "banana",1]
+# 변수 fruits의 두 번째 요소 출력
+print(fruits[2])        # orange
+# 변수 fruits의 마지막 요소 출력
+print(fruits[-1])       # 1

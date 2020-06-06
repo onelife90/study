9dd7b95e4d1e54ecd0fe18e282080db22b22b5af
jieun-kim-debug <onelife90@naver.com>
2020-06-06 11:14:36 +0900

    Create p116_list_in_list.py

---
+++ homework/딥러닝교과서/p116_list_in_list.py
@@ -0,0 +1,14 @@
+# 리스트의 오브젝트로 리스형을 저장 가능
+# ex) [[1,2,], [3,4]]
+# 중첩된 구조로 shape가 중요해짐
+
+fruit_name_1 = "사과"
+fruit_num_1 = 2
+fruit_name_2 = "귤"
+fruit_num_2 = 10
+
+# [["사과",2], ["귤",10]]이 출력되도록 fruits에 변수를 리스트형으로 대입
+
+fruits = [[fruit_name_1, fruit_num_1], [fruit_name_2, fruit_num_2]]
+print(fruits)
+# [['사과', 2], ['귤', 10]]

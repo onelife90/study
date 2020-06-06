4a727789f3e904986294a9990572691a555a1b9f
jieun-kim-debug <onelife90@naver.com>
2020-06-07 08:12:10 +0900

    Update and rename P206_axis.py to p206_array_2d_axis.py

--- homework/딥러닝교과서/P206_axis.py
+++
@@ -1,9 +0,0 @@
-# 2차원 배열에서는 axis라는 개념이 중요!
-# 열마다 처리하는 축 axis=0, 행마다 처리하는 축 axis=1
-
-import numpy as np
-
-arr = np.array([[1,2,3], [4,5,12], [15,20,22]])
-
-# arr 행의 합을 구하여 제시한 1차원 배열을 반환
-print(arr.sum(axis=1))

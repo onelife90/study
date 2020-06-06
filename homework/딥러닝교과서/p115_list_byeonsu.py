e46152231e0ff75ae25bc65c043dfa1c0de70fa5
jieun-kim-debug <onelife90@naver.com>
2020-06-06 11:04:03 +0900

    Create p115_list_byeonsu.py

---
+++ homework/딥러닝교과서/p115_list_byeonsu.py
@@ -0,0 +1,10 @@
+# 리스트 안에 다른 자료형, 변수를 리스트 안에 저장도 가능
+
+apple = 4
+grape = 3
+banana = 6
+
+# 리스트형 fruits 변수를 만들어 apple,grape,banana 변수를 요소로 저장
+
+fruits = [apple,grape,banana]
+print(fruits)       # [4, 3, 6]

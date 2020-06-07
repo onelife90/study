da4401058e6a8d164ceee0fc007878859fdd0032
jieun-kim-debug <onelife90@naver.com>
2020-06-08 08:48:53 +0900

    Update and rename keras23_ensemble3.py to keras23_ensemble3_in1_out2.py

--- keras/keras23_ensemble3_in1_out2.py
+++ keras/keras23_ensemble3_in1_out2.py
@@ -19,14 +19,7 @@ y1 = np.transpose(y1)
 # print(y1.shape)
 
 from sklearn.model_selection import train_test_split
-x1_train, x1_test, x2_train, x2_test, y1_train, y1_test = train_test_split(
-    x1,x2,y1, shuffle=False, train_size=0.8
-)       
-
-# from sklearn.model_selection import train_test_split
-# x2_train, x2_test, y1_train, y1_test = train_test_split(
-#     x2,y1, shuffle=False, train_size=0.8
-# )     
+x1_train, x1_test, x2_train, x2_test, y1_train, y1_test = train_test_split(x1,x2,y1, shuffle=False, train_size=0.8)       
 
 #2. 모델구성
 from keras.models import Sequential, Model
@@ -59,7 +52,6 @@ middle1 = Dense(40)(middle1)
 # output이 y1이므로 총 1개로 도출되어야 함
 
 ##### output 모델 구성 #####
-
 output1 = Dense(50)(middle1) # y_M1의 가장 끝 레이어가 middle1
 output1_2 = Dense(250)(output1)
 output1_3 = Dense(400)(output1_2)
@@ -76,14 +68,11 @@ model.summary()
 
 #3. 훈련
 model.compile(loss='mse', optimizer='adam', metrics=['mse']) 
-model.fit([x1_train, x2_train],
-          y1_train, epochs=80, batch_size=1,
-          validation_split=0.3, verbose=1)
+model.fit([x1_train, x2_train], y1_train, epochs=80, batch_size=1, validation_split=0.3, verbose=1)
           # list로 묶어서 한번에 model.fit 완성
                    
 #4. 평가, 예측
-loss = model.evaluate([x1_test, x2_test],
-                           y1_test, batch_size=1)
+loss = model.evaluate([x1_test, x2_test], y1_test, batch_size=1)
 # y1_output1에 대한 loss(1)
 # y1_output1에 대한 mse(1)
 # 총 2개의 반환값
@@ -91,11 +80,9 @@ loss = model.evaluate([x1_test, x2_test],
 print("loss : ", loss)
 
 y1_predict = model.predict([x1_test, x2_test])
-#(20,3)짜리 3개 왜? train_size=0.8이기 때문에
 print(y1_predict)
 
 # RMSE 구하기
-
 from sklearn.metrics import mean_squared_error
 def RMSE(y_test, y_predict): 
     return np.sqrt(mean_squared_error(y_test, y_predict)) 
@@ -121,5 +108,5 @@ print("R2 : ", R2)
 
 # epochs=1000, input1노드=3,80,500,400,100,200 input2노드=400,200,300,100,50,5 
 # middle노드= 100,80,60,70,90,40 output1노드=50,250,400,30,100,3
-#RMSE : 8.02/ /
-#R2 : 0.91/  / 
+#RMSE : 8.02/
+#R2 : 0.91/ 

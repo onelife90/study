0776bb4f24217358120707ae914ef34b63fd730f
jieun-kim-debug <onelife90@naver.com>
2020-06-08 12:51:36 +0900

    Update and rename keras/keras31_GRU.py to keras/complete/keras31_GRU.py

--- keras/complete/keras31_GRU.py
+++ keras/complete/keras31_GRU.py
@@ -1,15 +1,16 @@
+# GRU도 3차원 구성!
+
 from numpy import array
 from keras.models import Sequential
 from keras.layers import Dense, GRU
 #1. 데이터
 x = array([[1,2,3],[2,3,4],[3,4,5],[4,5,6]])
-y = array([4,5,6,7])        #(4, )
-
-print("x.shape : ", x.shape)        #(4,3)
-print("y.shape : ", y.shape)        #(4,)
+y = array([4,5,6,7])        
+# print("x.shape : ", x.shape)        #(4,3)
+# print("y.shape : ", y.shape)        #(4,)
 
 x = x.reshape(x.shape[0], x.shape[1], 1)    #x.shape[0]=4, x.shape[1]=3, 1
-print(x.shape)          #(4,3,1)
+# print(x.shape)          #(4,3,1)
 
 #2. 모델구성
 model = Sequential()
@@ -18,7 +19,7 @@ model.add(Dense(9000))
 model.add(Dense(300))
 model.add(Dense(1))
 
-model.summary()
+# model.summary()
 
 #3. 훈련
 model.compile(optimizer='adam', loss='mse')

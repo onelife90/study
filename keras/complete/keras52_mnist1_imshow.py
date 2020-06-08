ffd09325d6048966ed9ab35667bc83d395e583fb
jieun-kim-debug <onelife90@naver.com>
2020-06-08 20:57:22 +0900

    Update and rename keras/keras52_mnist1_imshow.py to keras/complete/keras52_mnist1_imshow.py

--- keras/complete/keras52_mnist1_imshow.py
+++ keras/complete/keras52_mnist1_imshow.py
@@ -6,16 +6,16 @@ from keras.datasets import mnist
 
 print(x_train[0])
 # 0~255의 숫자가 컬럼으로 찍혀있다
-print("y_train: ", y_train[0])
+# print("y_train: ", y_train[0])  # y_train:  5
 
-print(x_train.shape)        # (60000, 28, 28)
-print(x_test.shape)         # (10000, 28, 28)
-print(y_train.shape)        # (60000,)
-print(y_test.shape)         # (10000,)
+# print(x_train.shape)        # (60000, 28, 28)
+# print(x_test.shape)         # (10000, 28, 28)
+# print(y_train.shape)        # (60000,)
+# print(y_test.shape)         # (10000,)
 
 # plt.imshow(x_train[0], 'gray')
 # plt.imshow(x_train[0])    # 랜덤색깔
-print(x_train[0].shape)
+# print(x_train[0].shape)   # (28, 28)
 # plt.imshow(가로, 세로)==가로, 세로를 넣어주면 이미지를 출력
 # plt.show()
 

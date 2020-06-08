844ca04bffeb566de07b579228cca4f651ee5698
jieun-kim-debug <onelife90@naver.com>
2020-06-08 13:49:04 +0900

    Update and rename keras31_GRU2.py to keras31_GRU2_x_pred_reshape.py

--- keras/keras31_GRU2_x_pred_reshape.py
+++ keras/keras31_GRU2_x_pred_reshape.py
@@ -9,16 +9,14 @@ x = array([[1,2,3],[2,3,4],[3,4,5],[4,5,6],
            [20,30,40], [30,40,50], [40,50,60]])
 y = array([4,5,6,7,8,9,10,11,12,13,50,60,70])
 x_predict = array([50,60,70])            
-
-print("x.shape : ", x.shape)        # (13,3)
-print("y.shape : ", y.shape)        # (13,)
-print("x_predict.shape : ", x_predict.shape)       # (3,)
+# print("x.shape : ", x.shape)                        # (13,3)
+# print("y.shape : ", y.shape)                        # (13,)
+# print("x_predict.shape : ", x_predict.shape)        # (3,)
 
 x = x.reshape(x.shape[0], x.shape[1], 1)   
 x_predict = x_predict.reshape(1,3,1)
-
-print(x.shape)          #(13,3,1)
-print(x_predict.shape)  #(1,3,1)
+# print(x.shape)          #(13,3,1)
+# print(x_predict.shape)  #(1,3,1)
 
 #2. 모델구성
 model = Sequential()
@@ -34,7 +32,6 @@ model.compile(optimizer='adam', loss='mse')
 from keras.callbacks import EarlyStopping
 early_stopping = EarlyStopping(monitor='loss', patience=5, mode='auto')
 model.fit(x,y, epochs=5000, callbacks=[early_stopping])
-
 print(x_predict)
 
 #4. 평가, 예측

b3123ff1ab91b900a0138af42a3106085ba371e9
jieun-kim-debug <onelife90@naver.com>
2020-06-10 12:45:03 +0900

    Update dacon01_start_lstm.py

--- dacon/dacon01_start_lstm.py
+++ dacon/dacon01_start_lstm.py
@@ -12,11 +12,7 @@ from sklearn.metrics import mean_absolute_error
 #1. csv 불러오기
 train = pd.read_csv('./data/dacon/comp1/train.csv', header=0, index_col=0)
 test = pd.read_csv('./data/dacon/comp1/test.csv', header=0, index_col=0)
-submission = pd.read_csv('./data/dacon/comp1/sample_submission.csv', header=0, index_col=0)
-# print("train.shape: ", train.shape)         # (10000, 75) : x_train, x_test
-# print("test.shape: ", test.shape)           # (10000, 71) : x_pred
-# test.csv에는 y값이 될 컬럼이 존재하지 않으므로 x_pred로 지정
-# print("submission.shape", submission.shape) # (10000, 4) : y_pred
+sunmission = pd.read_csv('./data/dacon/comp1/sample_submission.csv', header=0, index_col=0)
 
 #1-1. 데이터 결측치 제거
 # print(train.isnull().sum())
@@ -29,19 +25,15 @@ train = train.fillna(train.mean())
 # print(train.isnull().sum())
 test = test.interpolate()
 test = test.fillna(test.mean())
-# print(train)
-# print(test)
 
-# count_data = train.groupby("hhb")["hhb"].count()
-# count_data = train.groupby("hbo2")["hbo2"].count()
-# count_data = train.groupby("ca")["ca"].count()
-# count_data = train.groupby("na")["na"].count()
-# print(count_data)
-# 0값 처리...
+#1-2. 넘파이 불러오기
+train = np.load('./data/dacon/comp1/train.npy')
+test = np.load('./data/dacon/comp1/test.npy')
+submission = np.load('./data/dacon/comp1/submission.npy')
 
 #1-2. 데이터 자르기
-x = train.iloc[:, :71]
-y = train.iloc[:, -4:]
+x = train[:, :71]
+y = train[:, -4:]
 # print(x.shape)  # (10000, 71)
 x_pred = test
 # print(x_pred.shape) # (10000, 71)
@@ -67,9 +59,9 @@ x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,random_sta
 
 #2. 모델 구성
 input1 = Input(shape=(1,60))
-dense1 = LSTM(300, activation='relu', return_sequences=True)(input1)
-dense1 = LSTM(400, activation='relu')(dense1)
-dense1 = Dense(500)(dense1)
+dense1 = LSTM(30, activation='relu', return_sequences=True)(input1)
+dense1 = LSTM(40, activation='relu')(dense1)
+dense1 = Dense(500, activation='relu')(dense1)
 dense1 = Dropout(0.1)(dense1)
 dense1 = Dense(80, activation='relu')(dense1)
 output1 = Dense(4, activation='relu')(dense1)
@@ -81,7 +73,7 @@ model.compile(loss='mae', optimizer='adam', metrics=['mae'])
 early_stop = EarlyStopping(monitor='val_loss', patience=3, mode='auto')
 modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'       
 check_p = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True)
-model.fit(x_train, y_train, epochs=100, batch_size=10, validation_split=0.2, callbacks=[early_stop, check_p])
+model.fit(x_train, y_train, epochs=200, batch_size=10, validation_split=0.2, callbacks=[early_stop, check_p])
 
 #4. 평가, 예측
 loss, mae = model.evaluate(x_test, y_test, batch_size=10)
@@ -89,7 +81,9 @@ y_pred = model.predict(x_pred)
 print(y_pred)
 print("loss: ", loss)
 print("mae: ", mae)
+# loss:  1.760384011864662
+# mae:  1.760384202003479
 
 #5. submit할 파일 생성
-# y_pred = pd.DataFrame(y_pred, index=np.arange(10000,20000), columns=["hhb","hbo2","ca","na"])
-# y_pred.to_csv('./data/dacon/comp1/sample_submission.csv', header=["hhb","hbo2","ca","na"], index=True, index_label="id")
+y_pred = pd.DataFrame(y_pred, index=np.arange(10000,20000))
+y_pred.to_csv('./data/dacon/comp1/submission_lstm.csv', header=["hhb","hbo2","ca","na"], index=True, index_label="id")

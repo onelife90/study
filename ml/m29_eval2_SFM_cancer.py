e1eda3df63849f95212ab4e9303919cc67e6267e
jieun-kim-debug <onelife90@naver.com>
2020-06-22 12:54:07 +0900

    Update m29_eval2_SFM_cancer.py

--- ml/m29_eval2_SFM_cancer.py
+++ ml/m29_eval2_SFM_cancer.py
@@ -24,11 +24,11 @@ x, y = load_breast_cancer(return_X_y=True)
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.8, random_state=88)
 
 #2. 모델 구성
-model = XGBClassifier(n_estimators=3, n_jobs=-1)
+model = XGBClassifier(n_estimators=500, n_jobs=-1)
 
 #3. 훈련
 model.fit(x_train, y_train, verbose=True, eval_metric=["error", "logloss"], 
-        eval_set=[(x_train, y_train), (x_test, y_test)], early_stopping_rounds=3)
+        eval_set=[(x_train, y_train), (x_test, y_test)], early_stopping_rounds=50)
 
 #3-1. 컬럼수 만큼 돌 thresholds 생성
 thresholds = np.sort(model.feature_importances_)
@@ -43,10 +43,10 @@ for thresh in thresholds:
     selection_x_test = selection.transform(x_test)
     print(selection_x_train.shape)
 
-    selection_model = XGBClassifier(n_estimators=3, n_jobs=-1) 
+    selection_model = XGBClassifier(n_estimators=500, n_jobs=-1) 
 
     selection_model.fit(selection_x_train, y_train, verbose=True, eval_metric=["error", "logloss"], 
-                        eval_set=[(selection_x_train, y_train), (selection_x_test, y_test)], early_stopping_rounds=3)
+                        eval_set=[(selection_x_train, y_train), (selection_x_test, y_test)], early_stopping_rounds=50)
     
     y_pred = selection_model.predict(selection_x_test)
     

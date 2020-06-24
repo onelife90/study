a6b0483fd9a6ee2e100fd8eacd5b2b2889ec16ed
jieun-kim-debug <onelife90@naver.com>
2020-06-24 15:49:50 +0900

    Create p32_main_fuc.py

---
+++ python/p32_main_fuc.py
@@ -0,0 +1,18 @@
+import p31_sample
+# p31_sample
+# x = 111
+# def test():
+#     print("x : ", x)
+
+x = 222
+
+# main_func이라는 새함수 작성
+def main_func():
+    print("x : ", x)
+
+p31_sample.test()
+# x :  111
+# p31_sample의 test라는 함수를 호출했으므로 x=111
+
+main_func()
+# x :  222

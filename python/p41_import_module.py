8fb667fb709d70cc9f31504d9ce36e108cdf3f11
jieun-kim-debug <onelife90@naver.com>
2020-06-24 16:00:09 +0900

    Create p41_import_module.py

---
+++ python/p41_import_module.py
@@ -0,0 +1,9 @@
+# machine이라는 모듈에 car_drive_hamsu라는 함수 패키지 import
+import machine.car_drive_hamsu
+import machine.tv_watch_hamsu
+
+machine.car_drive_hamsu.drive()
+machine.tv_watch_hamsu.watch()
+
+# 운전하다
+# 시청하다

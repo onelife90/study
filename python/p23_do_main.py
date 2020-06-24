a51149db1d31973cf87cf21013ad5d1222a654a6
jieun-kim-debug <onelife90@naver.com>
2020-06-24 15:46:05 +0900

    Create p23_do_main.py

---
+++ python/p23_do_main.py
@@ -0,0 +1,14 @@
+# if __name__ == '__main__':
+# import되는 경우
+
+import p21_car__main__
+import p22_tv__main__
+
+print("=============")
+
+p21_car__main__.drive()
+p22_tv__main__.watch()
+
+# =============
+# 운전하다
+# 시청하다

4920a796c590e4de26f8a553ca7906cf7e33e6e3
jieun-kim-debug <onelife90@naver.com>
2020-06-24 16:09:50 +0900

    Create p51_from_module.py

---
+++ python/p51_from_module.py
@@ -0,0 +1,46 @@
+from machine.car_drive_hamsu import drive
+from machine.tv_watch_hamsu import watch
+
+drive()
+watch()
+
+# 운전하다
+# 시청하다
+
+from machine import car_drive_hamsu
+from machine import tv_watch_hamsu
+
+car_drive_hamsu.drive()
+tv_watch_hamsu.watch()
+
+# 운전하다
+# 시청하다
+
+print("====================")
+
+from machine.test.car import drive
+from machine.test.tv import watch
+
+drive()
+watch()
+
+# 운전하다
+# 시청하다
+
+from machine.test import car
+from machine.test import tv
+
+car.drive()
+tv.watch()
+
+# 운전하다
+# 시청하다
+
+from machine import test
+from machine import test
+
+test.car.drive()
+test.tv.watch()
+
+# 운전하다
+# 시청하다

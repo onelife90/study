df6b60d422175d61532ad72626647fed058258c0
jieun-kim-debug <onelife90@naver.com>
2020-06-24 16:32:46 +0900

    Create p62_import.py

---
+++ python/p62_import.py
@@ -0,0 +1,17 @@
+'''
+패스 경로중
+'C:\\Users\\bitcamp\\anaconda3'
+이곳에 이 파일을 넣을 것이다
+'''
+
+print("이 import는 아나콘다 폴더에 들어있다")
+
+def sum2():
+    print("작업그룹 임포트 썸탄다")
+
+from test_import import p62_import
+p62_import.sum2()
+
+# 이 import는 아나콘다 폴더에 들어있다
+# 이 import는 아나콘다 폴더에 들어있다
+# 작업그룹 임포트 썸탄다

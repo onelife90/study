c265c131c18a095e5182586c61e32aece4465101
jieun-kim-debug <onelife90@naver.com>
2020-06-25 18:04:02 +0900

    Create 10869_yeonsan.py

---
+++ Coding_practice/Backjoon/10869_yeonsan.py
@@ -0,0 +1,10 @@
+# 10869 문제
+# 입력 : 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+# 출력 : 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+
+a, b = map(int, input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(int(a/b)) # 몫이 나와야 하기때문에 int를 사용
+print(a%b)

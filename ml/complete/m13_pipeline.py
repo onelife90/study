7d217c026d6c8b61c4cff50b75b148f37682ad2b
jieun-kim-debug <onelife90@naver.com>
2020-07-01 15:20:17 +0900

    Update and rename ml/m13_pipeline.py to ml/complete/m13_pipeline.py

--- ml/complete/m13_pipeline.py
+++ ml/complete/m13_pipeline.py
@@ -13,13 +13,15 @@ y = iris.target
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,random_state=88)
 
 #2. 모델 구성
-# model = SVC()
-# svc_model = SVC()
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
-pipe = Pipeline([("scaler", MinMaxScaler()), ("svm", SVC())])
+# pipe = Pipeline([("scaler", MinMaxScaler()), ("svm", SVC())])
 # SVC 모델과 MinMaxScaler를 쓰겠다
+pipe = make_pipeline(MinMaxScaler(), SVC())
+
+#3. 훈련
+# 전처리와 모델을 한꺼번에 처리하는 것이 pipe이기 때문에 pipe.fit
 pipe.fit(x_train, y_train)
 print("acc: ", pipe.score(x_test, y_test))
 # acc:  0.9666666666666667

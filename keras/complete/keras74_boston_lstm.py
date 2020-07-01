9d59fea15cb4eef07a588bac37da72e057ac632b
jieun-kim-debug <onelife90@naver.com>
2020-07-01 10:07:29 +0900

    Update and rename keras/keras74_boston_lstm.py to keras/complete/keras74_boston_lstm.py

--- keras/complete/keras74_boston_lstm.py
+++ keras/complete/keras74_boston_lstm.py
@@ -27,12 +27,14 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.6)
 
 #2. 모델 구성
 input1 = Input(shape=(13,1))
-dense1 = LSTM(10, return_sequences=True)(input1)
-dense1 = LSTM(1)(dense1)
-dense1 = Dense(5000)(dense1)
-dense1 = Dropout(0.2)(dense1)
-dense1 = Dense(500)(dense1)
-dense1 = Dense(300)(dense1)
+dense1 = LSTM(13, return_sequences=True)(input1)
+dense1 = LSTM(26)(dense1)
+dense1 = Dense(52)(dense1)
+dense1 = Dense(78)(dense1)
+dense1 = Dense(104)(dense1)
+dense1 = Dense(91)(dense1)
+dense1 = Dense(65)(dense1)
+dense1 = Dense(39)(dense1)
 output1 = Dense(1)(dense1)
 
 model = Model(inputs=input1, outputs=output1)
@@ -40,10 +42,9 @@ model = Model(inputs=input1, outputs=output1)
 #3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam', metrics=['mse'])
 earlystopping = EarlyStopping(monitor='mse', patience=5, mode='auto')
-modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
+modelpath = './model/boston/{epoch:02d}-{val_loss:.4f}.hdf5'
 checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True, mode='auto')
-tb_hist = TensorBoard(log_dir='graph', histogram_freq=0, write_graph=True, write_images=True)
-hist = model.fit(x_train, y_train, epochs=100, batch_size=1, validation_split=0.2, callbacks=[earlystopping, checkpoint, tb_hist])
+model.fit(x_train, y_train, epochs=100, batch_size=1, validation_split=0.2, callbacks=[earlystopping, checkpoint])
 
 #4. 평가, 예측
 loss, mse = model.evaluate(x_test, y_test, batch_size=1)
@@ -57,31 +58,6 @@ print("RMSE: ", RMSE(y_test, y_pred))
 #R2 구하기
 r2 = r2_score(y_test, y_pred)
 print("R2: ", r2)
-
-loss = hist.history['loss']
-mse = hist.history['mse']
-val_loss = hist.history['val_loss']
-val_mse = hist.history['val_mse']
-
-#5. 시각화
-plt.figure(figsize=(10,6))
-plt.subplot(2,1,1)
-plt.plot(loss, marker='.', c='red', label='loss')
-plt.plot(val_loss, marker='.', c='blue', label='val_loss')
-plt.title('loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend()
-
-plt.subplot(2,1,2)
-plt.plot(mse, marker='.', c='red', label='mse')
-plt.plot(val_mse, marker='.', c='blue', label='val_mse')
-plt.title('mse')
-plt.ylabel('mse')
-plt.xlabel('epoch')
-plt.legend()
-
-plt.show()
 
 # 튜닝
 # epochs=100,batch=1,노드=LSTM10,LSTM1,5000,drop0.2,500,300

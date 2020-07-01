bf3b4d41c73ef785b2c4a7de7b12c4d699403400
jieun-kim-debug <onelife90@naver.com>
2020-07-01 10:29:18 +0900

    Update keras78_iris_cnn.py

--- keras/keras78_iris_cnn.py
+++ keras/keras78_iris_cnn.py
@@ -25,22 +25,26 @@ x = x.reshape(-1, 2, 2, 1)
 # print(x.shape)        # (150,2,2,1)
 
 #1-2. train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=99, train_size=0.6)
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=99, train_size=0.8)
 
 #2. 모델 구성
 model = Sequential()
-model.add(Conv2D(5, (2,2), input_shape=(2,2,1)))
-model.add(Conv2D(1000, (2,2), padding='same'))
+model.add(Conv2D(4, (2,1), input_shape=(2,2,1)))
+model.add(Conv2D(8, (2,1), padding='same'))
+model.add(Conv2D(16, (2,1), padding='same'))
+model.add(Conv2D(24, (2,1), padding='same'))
+model.add(Conv2D(32, (2,1), padding='same'))
 model.add(MaxPooling2D(pool_size=2, padding='same'))
-model.add(Dropout(0.2))
-model.add(Conv2D(30, (2,2), padding='same'))
+model.add(Conv2D(28, (2,1), padding='same'))
+model.add(Conv2D(20, (2,1), padding='same'))
+model.add(Conv2D(12, (2,1), padding='same'))
 model.add(Flatten())
 model.add(Dense(3, activation='softmax'))
 
 #3. 컴파일, 훈련
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 early_stop = EarlyStopping(monitor='loss', patience=5, mode='auto')
-modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
+modelpath = './model/iris/{epoch:02d}-{val_loss:.4f}.hdf5'
 checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True, mode='auto')
 tb_hist = TensorBoard(log_dir='graph', histogram_freq=0, write_graph=True, write_images=True)
 hist = model.fit(x_train, y_train, epochs=100, batch_size=1, validation_split=0.2, callbacks=[early_stop, checkpoint, tb_hist])

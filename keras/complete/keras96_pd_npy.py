67278cc61d46262a657009c953388ca7a4ba1384
jieun-kim-debug <onelife90@naver.com>
2020-07-01 11:22:30 +0900

    Update and rename keras/keras96_pd_npy.py to keras/complete/keras96_pd_npy.py

--- keras/complete/keras96_pd_npy.py
+++ keras/complete/keras96_pd_npy.py
@@ -10,19 +10,16 @@ import matplotlib.pyplot as plt
 datasets = pd.read_csv("./data/csv/iris.csv", index_col=None, header=0, sep=',')
 # index_col=None 읽기 전 파일에 index_col에 데이터가 껴있었다. 그래서 None
 # header=0을 하면 첫 헤더(행)는 실 데이터로 인식 X
-# print(datasets)
 
 # print(datasets.head())      # 위에서부터 5개
 # print(datasets.tail())      # 아래서부터 5개
 
-print("======================")
 # print(datasets.values)      # ★항상 쓰임. 머신을 돌리기 위해서 np로 변환
 
 aaa = datasets.values
 # print(type(aaa))            # <class 'numpy.ndarray'>
 
 # np로 저장하시오
-
 from sklearn.datasets import load_iris
 
 iris = load_iris()
@@ -45,17 +42,12 @@ y = aaa[:, 4]
 y = np_utils.to_categorical(y)
 # print(y.shape)      # (150, 3)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.6, random_state=99, shuffle=True)
-
-# print(x_train.shape)        # (90, 4)
-# print(x_test.shape)         # (60, 4)
-# print(y_train.shape)        # (90, 3)
-# print(y_test.shape)         # (60, 3)
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=99, shuffle=True)
 
 #2. 모델 구성
 input1 = Input(shape=(4, ))
-dense1 = Dense(100, activation='softmax')(input1)
-dense1 = Dense(80)(dense1)
+dense1 = Dense(3, activation='softmax')(input1)
+dense1 = Dense(6)(dense1)
 dense1 = Dense(60)(dense1)
 dense1 = Dropout(0.1)(dense1)
 dense1 = Dense(30)(dense1)
@@ -68,10 +60,10 @@ model = Model(inputs=input1, outputs=output1)
 #3. 컴파일, 훈련
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 early_stop = EarlyStopping(monitor='loss', patience=4, mode='auto')
-modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
+modelpath = './model/iris/{epoch:02d}-{val_loss:.4f}.hdf5'
 checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True, save_weights_only=False)
 tb_hist = TensorBoard(log_dir='graph', histogram_freq=0, write_graph=True, write_images=True)
-hist = model.fit(x_train, y_train, epochs=1000, batch_size=1, validation_split=0.2, callbacks=[early_stop, checkpoint, tb_hist])
+hist = model.fit(x_train, y_train, epochs=100, batch_size=1, validation_split=0.2, callbacks=[early_stop, checkpoint, tb_hist])
 
 #4. 평가, 예측
 loss, acc = model.evaluate(x_test, y_test, batch_size=1)
@@ -103,10 +95,6 @@ plt.legend()
 plt.show()
 
 # 튜닝
-# epochs=16,batch=1,노드=
-#loss:  0.11906453711269478
-#acc:  0.9333333373069763
-
 # epochs=22,batch=1,노드=LSTM10,LSTM5,drop0.1,80,60,drop0.1,30,10,5
 #loss:  0.18599484871398697
 #acc:  0.9666666388511658

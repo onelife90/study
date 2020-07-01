9f1ffe385e8373c2a2dd81aa505bb1177e69a94f
jieun-kim-debug <onelife90@naver.com>
2020-07-01 10:45:54 +0900

    Update and rename keras/keras84_breastCancer_cnn.py to keras/complete/keras84_breastCancer_cnn.py

--- keras/complete/keras84_breastCancer_cnn.py
+++ keras/complete/keras84_breastCancer_cnn.py
@@ -24,29 +24,31 @@ y = np_utils.to_categorical(y)
 x = x.reshape(-1,6,5,1)
 # print(x.shape)      # (596,6,5,1)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=77, train_size=0.6)
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=77, train_size=0.8)
 
 #2. 모델 구성
 model = Sequential()
-model.add(Conv2D(5000, (2,2), padding='same', input_shape=(6,5,1)))
-model.add(Conv2D(3000, (2,2), padding='same', activation='softmax'))
-model.add(Conv2D(9000, (2,2), padding='same', activation='softmax'))
-model.add(MaxPooling2D(pool_size=2, padding='same'))
+model.add(Conv2D(30, (2,2), padding='same', input_shape=(6,5,1)))
+model.add(Conv2D(60, (2,2), padding='same', activation='softmax'))
+model.add(Conv2D(120, (2,2), padding='same', activation='softmax'))
+model.add(Conv2D(180, (2,2), padding='same', activation='softmax'))
+model.add(Conv2D(240, (2,2), padding='same', activation='softmax'))
+model.add(MaxPooling2D(pool_size=3, padding='same'))
 model.add(Dropout(0.1))
-model.add(Conv2D(400, (2,2), padding='same', activation='softmax'))
-model.add(Conv2D(200, (2,2), padding='same', activation='softmax'))
+model.add(Conv2D(210, (2,2), padding='same', activation='softmax'))
+model.add(Conv2D(150, (2,2), padding='same', activation='softmax'))
+model.add(Conv2D(90, (2,2), padding='same', activation='softmax'))
 model.add(MaxPooling2D(pool_size=2, padding='same'))
-model.add(Conv2D(10, (2,2), padding='same', activation='softmax'))
 model.add(Flatten())
 model.add(Dense(2, activation='softmax'))
 
 #3. 컴파일, 훈련
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 early_stop = EarlyStopping(monitor='loss', patience=5, mode='auto')
-modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
+modelpath = './model/cancer/{epoch:02d}-{val_loss:.4f}.hdf5'
 checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True)
 tb_hist = TensorBoard(log_dir='graph', histogram_freq=0,  write_graph=True, write_images=True)
-hist = model.fit(x_train, y_train, epochs=500, batch_size=1, validation_split=0.2, callbacks=[early_stop, checkpoint, tb_hist])
+hist = model.fit(x_train, y_train, epochs=100, batch_size=1, validation_split=0.2, callbacks=[early_stop, checkpoint, tb_hist])
 
 #4. 평가, 예측
 loss, acc = model.evaluate(x_test, y_test, batch_size=1)
@@ -78,8 +80,3 @@ plt.ylabel('acc')
 plt.xlabel('epoch')
 plt.legend()
 plt.show()
-
-# 튜닝
-# epochs=12,batch=1,노드=5000,3000,9000,max2,drop0.1,400,200,max2,10,flat
-#loss:  0.6581239072900069
-#acc:  0.6315789222717285

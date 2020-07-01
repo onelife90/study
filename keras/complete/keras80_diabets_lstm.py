a7d71c29591bc651a10d99455cfd501d149f0cc7
jieun-kim-debug <onelife90@naver.com>
2020-07-01 10:34:11 +0900

    Update and rename keras/keras80_diabets_lstm.py to keras/complete/keras80_diabets_lstm.py

--- keras/complete/keras80_diabets_lstm.py
+++ keras/complete/keras80_diabets_lstm.py
@@ -31,12 +31,15 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=99, train
 
 #2. 모델
 input1 = Input(shape=(5,2))
-dense1 = LSTM(300)(input1)
-dense1 = Dense(7000)(dense1)
-dense1 = Dropout(0.3)(dense1)
-dense1 = Dense(3000)(dense1)
-dense1 = Dense(100)(dense1)
-dense1 = Dropout(0.1)(dense1)
+dense1 = LSTM(10)(input1)
+dense1 = Dense(20)(dense1)
+dense1 = Dense(40)(dense1)
+dense1 = Dense(60)(dense1)
+dense1 = Dense(80)(dense1)
+dense1 = Dense(70)(dense1)
+dense1 = Dense(50)(dense1)
+dense1 = Dense(30)(dense1)
+dense1 = Dense(10)(dense1)
 output1 = Dense(1)(dense1)
 
 model = Model(inputs=input1, outputs=output1)
@@ -44,10 +47,9 @@ model = Model(inputs=input1, outputs=output1)
 #3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam', metrics=['mse'])
 early_stop = EarlyStopping(monitor='loss', patience=5, mode='auto')
-modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
+modelpath = './model/cancer/{epoch:02d}-{val_loss:.4f}.hdf5'
 checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True, mode='auto')
-tb_hist = TensorBoard(log_dir='graph', histogram_freq=0, write_graph=True, write_images=True)
-hist = model.fit(x_train, y_train, epochs=5000, batch_size=1, validation_split=0.2, callbacks=[early_stop, checkpoint, tb_hist])
+model.fit(x_train, y_train, epochs=5000, batch_size=1, validation_split=0.2, callbacks=[early_stop, checkpoint])
 
 #4. 평가, 예측
 loss, mse = model.evaluate(x_test, y_test, batch_size=1)
@@ -61,31 +63,6 @@ print("RMSE: ", RMSE(y_test, y_pred))
 #R2 구하기
 r2 = r2_score(y_test, y_pred)
 print("R2: ", r2)
-
-loss = hist.history['loss']
-mse = hist.history['mse']
-val_loss = hist.history['val_loss']
-val_mse = hist.history['val_mse']
-
-#5. 시각화
-plt.figure(figsize=(10,6))
-plt.subplot(2,1,1)
-plt.plot(loss, marker='.', c='red', label='loss')
-plt.plot(val_loss, marker='.', c='blue', label='val_loss')
-plt.title('loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend()
-
-plt.subplot(2,1,2)
-plt.plot(mse, marker='.', c='red', label='mse')
-plt.plot(val_mse, marker='.', c='blue', label='val_mse')
-plt.title('mse')
-plt.ylabel('mse')
-plt.xlabel('epoch')
-plt.legend()
-
-plt.show()
 
 # 튜닝
 # epochs=25,batch=1,노드=LSTM300,7000,drop0.3,3000,100,drop0.1

668bd55e65c7f12a55ada1a839229edc3e415efb
jieun-kim-debug <onelife90@naver.com>
2020-07-01 12:06:13 +0900

    Update and rename keras/keras98_randomsearch.py to keras/complete/keras98_randomsearch.py

--- keras/complete/keras98_randomsearch.py
+++ keras/complete/keras98_randomsearch.py
@@ -30,7 +30,7 @@ y_test = np_utils.to_categorical(y_test)
 #2-1. 모델 구성(함수)
 # 그리드서치를 사용할 것인데, 사이킷런에 있는 것이기 때문에 모델을 만들어야 함
 # model = RandomizedSearchCV(RandomForestClassifier(), parameters, cv=kfold, n_jobs=-1)
-# 그리드서치의 첫번재 매개 변수가 model이기 때문에 모델 자체를 함수로 만들겠다
+# 그리드서치의 첫번째 매개 변수가 model이기 때문에 모델 자체를 함수로 만들겠다
 def build_model(drop=0.5, optimizer='adam'):
     inputs = Input(shape=(28*28, ), name='input')
     x = Dense(512, activation='relu', name='hidden1')(inputs)
@@ -43,8 +43,9 @@ def build_model(drop=0.5, optimizer='adam'):
     model = Model(inputs=inputs, outputs=outputs)
     model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['acc'])
     return model
-# 모델 만든 함수에서는 compile까지만, fit은 그리드서치에서 실행(cv가 있기 때문에)
-# 사이킷런에 쓸수 있게 KerasClassifier를 wrapping 한 것(모델, 파라미터, cv을 wrap해서 사용하겠다)
+    # 모델 만든 함수에서는 compile까지만, fit은 그리드서치에서 실행(cv가 있기 때문에)
+    # 사이킷런에 쓸수 있게 KerasClassifier를 wrapping 한 것(모델, 파라미터, cv을 wrap해서 사용하겠다)
+
 # 그리드서치에 들어갈 첫번째 모델 구성
 from keras.wrappers.scikit_learn import KerasClassifier
 model = KerasClassifier(build_fn=build_model, verbose=1)
@@ -57,14 +58,18 @@ def create_hyperparameters():
     optimizers = ['rmsprop', 'adam', 'adadelta']
     dropout = np.linspace(0.1,0.5,5)
     return{"batch_size": batches, "optimizer":optimizers, "drop": dropout}
+    # 하이퍼파라미터에 대한 매개변수들이 key, values라 딕셔너리형으로 return
 hyperparameters = create_hyperparameters()
-# 하이퍼파라미터에 대한 매개변수들이 key, values라 딕셔너리형으로 return
 
 #2-3. 그리드서치 만들기
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 search = RandomizedSearchCV(model, hyperparameters, cv=3)
+
 search.fit(x_train, y_train)
 print(search.best_params_)
-# {'optimizer': 'adadelta', 'drop': 0.30000000000000004, 'batch_size': 100}
-y_pred = model.evaluate(x_test)
-print("acc_score: ", accuracy_score(y_test, y_pred))
+# {'optimizer': 'rmsprop', 'drop': 0.30000000000000004, 'batch_size': 200}
+# y_pred = model.evaluate(x_test)
+
+acc = search.score(x_test, y_test)
+print("acc: ", acc)
+# acc:  0.9815999865531921

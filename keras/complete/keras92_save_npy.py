f0460714b1ae55f1c4f7084a9f53dfe1fd1ea330
jieun-kim-debug <onelife90@naver.com>
2020-07-01 11:09:27 +0900

    Update and rename keras/keras92_save_npy.py to keras/complete/keras92_save_npy.py

--- keras/complete/keras92_save_npy.py
+++ keras/complete/keras92_save_npy.py
@@ -2,18 +2,18 @@ import numpy as np
 from sklearn.datasets import load_iris
 
 iris = load_iris()
-
 # print(type(iris))       # <class 'sklearn.utils.Bunch'>
 
 x_data = iris.data
 y_data = iris.target
-
 # print(type(x_data))     # <class 'numpy.ndarray'>
 # print(type(y_data))     # <class 'numpy.ndarray'>
 
+# save 할 때는 arr(배열 형태로 저장, 변수명)
 np.save('./data/iris_x.npy', arr=x_data)
 np.save('./data/iris_y.npy', arr=y_data)
 
+# load 할 때는 경로만 입력
 x_data_load = np.load('./data/iris_x.npy')
 y_data_load = np.load('./data/iris_y.npy')
 

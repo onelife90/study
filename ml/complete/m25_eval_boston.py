5b9c4753cc52fd82c36788fa913a004244383d98
jieun-kim-debug <onelife90@naver.com>
2020-07-01 16:44:15 +0900

    Update and rename ml/m25_eval_boston.py to ml/complete/m25_eval_boston.py

--- ml/complete/m25_eval_boston.py
+++ ml/complete/m25_eval_boston.py
@@ -9,16 +9,16 @@ from sklearn.metrics import r2_score
 
 #1. 데이터
 x, y = load_boston(return_X_y=True)
-x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.8, random_state=99)
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state=99)
 
 #2. 모델 구성
 model = XGBRegressor(n_estimators=1000, learning_rate=0.1)
 # n_estimators는 딥러닝의 epochs와 같음
 
 #3. 훈련
-model.fit(x_train, y_train, verbose=True, eval_metric="rmse", eval_set=[(x_train,y_train), (x_test, y_test)],
+model.fit(x_train, y_train, verbose=False, eval_metric="rmse", eval_set=[(x_train,y_train), (x_test, y_test)],
          early_stopping_rounds=20)
-# verbose 딥러닝의 metrics가 있었음. 머신러닝의 지표는 rmse, mae, logloss, error(=acc), auc(정확도 acc의 친구)
+# 딥러닝의 metrics가 있었음. 머신러닝의 지표는 rmse, mae, logloss, error(<=>acc), auc(정확도 acc의 친구)
 # error가 0.8이면 acc가 0.2
 
 #4. 평가

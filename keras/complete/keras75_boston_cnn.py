563b35e497dbf2e6f416642a6da80e634d82402a
jieun-kim-debug <onelife90@naver.com>
2020-07-01 10:21:13 +0900

    Update and rename keras/keras75_boston_cnn.py to keras/complete/keras75_boston_cnn.py

--- keras/complete/keras75_boston_cnn.py
+++ keras/complete/keras75_boston_cnn.py
@@ -32,22 +32,27 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=66, train
 
 #2. 모델 구성
 input1 = Input(shape=(4,2,1))
-dense1 = Conv2D(169, (2,2), padding='same')(input1)
-dense1 = Conv2D(303, (2,2), padding='same')(dense1)
-dense1 = MaxPooling2D(pool_size=2)(dense1)
-dense1 = Conv2D(303, (2,2), padding='same')(dense1)
+dense1 = Conv2D(32, (2,2), padding='same')(input1)
+dense1 = Conv2D(64, (2,2), padding='same')(dense1)
+dense1 = Conv2D(128, (2,2), padding='same')(dense1)
+dense1 = Conv2D(256, (2,2), padding='same')(dense1)
+dense1 = MaxPooling2D(pool_size=3, padding='same')(dense1)
+dense1 = Dropout(0.2)(dense1)
+dense1 = Conv2D(224, (2,1), padding='same')(dense1)
+dense1 = Conv2D(160, (2,1), padding='same')(dense1)
+dense1 = Conv2D(69, (2,1), padding='same')(dense1)
+dense1 = MaxPooling2D(pool_size=2, padding='same')(dense1)
 dense1 = Flatten()(dense1)
-output1 = Dense(1)(dense1)
+output1 = Dense(10)(dense1)
 
 model = Model(inputs=input1, outputs=output1)
 
 #3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam', metrics=['mse'])
 earlystopping = EarlyStopping(monitor='mse', patience=5, mode='auto')
-modelpath = './model/{epoch:02d}-{val_loss:.4f}.hdf5'
+modelpath = './model/boston/{epoch:02d}-{val_loss:.4f}.hdf5'
 checkpoint = ModelCheckpoint(filepath=modelpath, monitor='val_loss', save_best_only=True, mode='auto')
-tb_hist = TensorBoard(log_dir='graph', histogram_freq=0, write_graph=True, write_images=True)
-hist = model.fit(x_train, y_train, epochs=100, batch_size=1, validation_split=0.2, callbacks=[earlystopping, checkpoint, tb_hist])
+model.fit(x_train, y_train, epochs=100, batch_size=1, validation_split=0.2, callbacks=[earlystopping, checkpoint])
 
 #4. 평가, 예측
 loss, mse = model.evaluate(x_test, y_test, batch_size=1)
@@ -58,35 +63,9 @@ y_predict = model.predict(x_test)
 from sklearn.metrics import mean_squared_error
 def RMSE(y_test, y_predict):
     return np.sqrt(mean_squared_error(y_test, y_predict))
-print("RMSE: ", RMSE)
+print("RMSE: ", RMSE(y_test, y_predict))
 
 # R2 구하기
 from sklearn.metrics import r2_score
 r2 = r2_score(y_test, y_predict)
 print("r2: ", r2)
-
-loss = hist.history['loss']
-mse = hist.history['mse']
-val_loss = hist.history['val_loss']
-val_mse = hist.history['val_mse']
-
-#5. 시각화
-plt.figure(figsize=(10,6))
-plt.subplot(2,1,1)
-plt.plot(loss, marker='.', c='red', label='loss')
-plt.plot(val_loss, marker='.', c='blue', label='val_loss')
-plt.grid()
-plt.title('loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend()
-
-plt.subplot(2,1,2)
-plt.plot(mse, marker='.', c='red', label='mse')
-plt.plot(val_mse, marker='.', c='blue', label='val_mse')
-plt.grid()
-plt.title('mse')
-plt.ylabel('mse')
-plt.xlabel('epoch')
-plt.legend()
-plt.show()

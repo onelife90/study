8270d4c9b490a0e8e5d4da92cf7457e9a0fd076f
jieun-kim-debug <onelife90@naver.com>
2020-07-01 14:15:12 +0900

    Update and rename ml/m07_cancer.py to ml/complete/m07_cancer.py

--- ml/complete/m07_cancer.py
+++ ml/complete/m07_cancer.py
@@ -23,46 +23,42 @@ scaler = StandardScaler()
 x = scaler.fit_transform(x)
 # print(x.shape)      # (569,30)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=77, train_size=0.6)
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=77, train_size=0.8)
 
 #2. 모델 구성
 # model = SVC()                            
-# score:  0.9868421052631579
-# acc:  0.9868421052631579
-# r2:  0.9434523809523809
+# score:  0.9649122807017544
+# acc:  0.9649122807017544
 
 # model = LinearSVC()                   # 규제에 편향을 포함싴켜서 SVC보다 낮은 정확도                  
-# score:  0.9692982456140351
-# acc:  0.9692982456140351
-# r2:  0.8680555555555556
+# score:  0.9649122807017544
+# acc:  0.9649122807017544
 
 # model = RandomForestClassifier()         
-# score:  0.956140350877193
-# acc:  0.956140350877193
-# r2:  0.8115079365079365
+# score:  0.9298245614035088
+# acc:  0.9298245614035088
 
 # model = RandomForestRegressor()          
 # error
 
-# model = KNeighborsClassifier()           
+model = KNeighborsClassifier()           
 # score:  0.9649122807017544
 # acc:  0.9649122807017544
-# r2:  0.8492063492063492
 
-model = KNeighborsRegressor()            
+# model = KNeighborsRegressor()            
 # error
 
 #3. 컴파일, 훈련
-# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
-hist = model.fit(x_train, y_train)
+model.fit(x_train, y_train)
 y_pred = model.predict(x_test)
 
 #4. 평가, 예측
 # loss, acc = model.evaluate(x_test, y_test, batch_size=5)
+# model.evaluate(딥러닝)==model.score(머신러닝)
 score = model.score(x_test, y_test)
 acc = accuracy_score(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+# r2 = r2_score(y_test, y_pred)
 
 print("score: ", score)
 print("acc: ", acc)
-print("r2: ", r2)
+# print("r2: ", r2)

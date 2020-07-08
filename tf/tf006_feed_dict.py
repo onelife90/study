318569769b4f1d30b6d2689e1b3397679c0fa42f
jieun-kim-debug <onelife90@naver.com>
2020-07-08 20:50:51 +0900

    Create tf006_feed_dict.py

---
+++ tf/tf006_feed_dict.py
@@ -0,0 +1,51 @@
+# 텐서플로우는 그래프를 미리 만들어 놓고, 필요한 시점에 해당 그래프를 실행하는 지연실행 방식을 사용
+# 이에 걸 맞게 placeholder는 변수의 타입을 미리 설정해놓고 필요한 변수를 나중에 받아서 실행
+
+import tensorflow as tf
+
+#1-1. 그래프 수준의 난수 시드 설정
+# random_seed는 (graph, operation)의 두개 시드를 도출함. 여기서는 그래프 시드 도출. seed는 파이썬 정수
+tf.set_random_seed(777)
+
+#1-2. feed_dict에 feed 될 텐서를 위한 placeholder 설정
+x_train = tf.placeholder(tf.float32, shape=[None])
+y_train = tf.placeholder(tf.float32, shape=[None])
+# placeholder로 y_train에는 실수형에 텐서의 구조가 어떤지 모르는 값을 세팅
+# 어떤 인풋 값에 따라서 아웃풋 내용이 달라지기 때문에 값을 받아들이는 통로라고 생각하자
+
+# random_normal 정규분포로부터의 난수값 반환 ([1])=shape
+w = tf.Variable(tf.random_normal([1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+# sess = tf.Session()
+# sess.run(tf.global_variables_initializer())
+# print("w의 가중치: ", sess.run(w))
+# w의 가중치:  [2.2086694]
+
+h = x_train * w + b
+
+# 케라스 컴파일 시 가장 중요한 것 loss=(cost)
+cost = tf.reduce_mean(tf.square(h - y_train))
+# (h-y)**2의 합 / 개수 == mse
+
+# 케라스 컴파일 시 옵티마이저(경사하강법). minimize(cost)=cost가 가장 적을 때 구하라
+train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
+
+# with 문 안에 sess가 포함
+# with tf.Session() as sess:
+with tf.compat.v1.Session() as sess:
+    # 전체 변수들이 싹 초기화
+    sess.run(tf.global_variables_initializer())
+    for step in range(2001):
+        # _:공백(실행은 되지만 결과값 출력 안하겠다)
+        _, cost_val, w_val, b_val = sess.run([train, cost, w, b], feed_dict={x_train:[1,2,3], y_train:[3,5,7]})
+        if step%20==0:
+            print(step, cost_val, w_val, b_val)
+
+    # 예측해보자
+    print("4의 예측값: ", sess.run(h, feed_dict={x_train:[4]}))
+    # 4의 예측값:  [9.000078]
+    print("[5,6]의 예측값: ", sess.run(h, feed_dict={x_train:[5,6]}))
+    # [5,6]의 예측값:  [11.000123 13.000169]
+    print("[6,7,8]의 예측값: ", sess.run(h, feed_dict={x_train:[6,7,8]}))    
+    # [6,7,8]의 예측값:  [13.000169 15.000214 17.000257]

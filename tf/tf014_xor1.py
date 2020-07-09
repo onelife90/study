cf74c599cde56355131531d61e261d3a268ae8b4
jieun-kim-debug <onelife90@naver.com>
2020-07-09 20:56:10 +0900

    Create tf014_xor1.py

---
+++ tf/tf014_xor1.py
@@ -0,0 +1,51 @@
+# x,y,w,b, h,cost,opt
+# sigmoid 사용
+# pred, acc 준비
+
+import tensorflow as tf
+import numpy as np
+
+#1-1. 그래프 수준의 난수 시드 설정
+tf.set_random_seed(777)
+
+#1-2. 데이터
+x_data = np.array([[0,0],[0,1],[1,0],[1,1]], dtype=np.float32)
+y_data = np.array([[0],[1],[1],[0]], dtype=np.float32)
+
+#1-3. feed_dict에 feed 될 텐서를 위한 placeholder 설정
+x = tf.placeholder('float32', [None, 2])
+y = tf.placeholder('float32', [None, 1])
+
+#2. 모델 구성
+w = tf.Variable(tf.random_normal([2,1], name='weight'))
+b = tf.Variable(tf.random_normal([1], name='bias'))
+h = tf.matmul(x,w) + b
+
+#2-1. cost 손실함수(binary_crossentropy) 정의
+cost = -tf.reduce_mean(y*tf.log(h)+(1-y)*tf.log(1-h))
+
+#2-2. loss를 최소화하는 옵티마이저 정의
+opt = tf.train.GradientDescentOptimizer(learning_rate=1e-3).minimize(cost)
+
+#2-3. 예측
+pred = tf.cast(h>0.5, dtype=tf.float32)
+acc = tf.reduce_mean(tf.cast(tf.equal(pred, y), dtype=tf.float32))
+
+#3. 훈련
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for step in range(5001):
+        cost_val, _ = sess.run([cost, opt], feed_dict={x:x_data, y:y_data})
+    h,c,a = sess.run([h, pred, acc], feed_dict={x:x_data, y:y_data})
+    print("H: \n", h, "\n correct(y): \n", c, "\n acc: ", a)
+    # H:
+    # [[0.27415314]
+    # [2.247741  ]
+    # [2.3320005 ]
+    # [4.3055882 ]]
+    # correct(y):
+    # [[0.]
+    # [1.]
+    # [1.]
+    # [1.]]
+    # acc:  0.75

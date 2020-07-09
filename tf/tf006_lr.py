58bb6f93adac40eacfe54dfa165ec86d77df799d
jieun-kim-debug <onelife90@naver.com>
2020-07-09 19:13:38 +0900

    Update tf006_lr.py

--- tf/tf006_lr.py
+++ tf/tf006_lr.py
@@ -2,35 +2,31 @@
 # 0.01 -> 0.1 / 0.001 / 1
 # epoch가 2000보다 적게 만들어라
 
-# x,y를 정제된 데이터 말고 placeholder를 쓸 수 있지 않을까?
-
 import tensorflow as tf
-# 그래프 수준의 난수 시드 설정
+
+#1-1. 그래프 수준의 난수 시드 설정
 tf.set_random_seed(777)
 
+#1-2. feed_dict에 feed 될 텐서를 위한 placeholder 설정
 x_train = tf.placeholder(tf.float32, shape=[None])
 y_train = tf.placeholder(tf.float32, shape=[None])
 
-# random_normal 정규분포로부터의 난수값 반환 ([1])=shape
+#2. 모델 구성
+# random_normal 정규분포로부터의 난수값 반환. ([1])=shape
+# w, b는 인풋과 아웃풋 값을 기반으로 변경이 되며 인공지능은 바로 w를 예측하는 것이 핵심이기에 변수로 선언하여 구현
 w = tf.Variable(tf.random_normal([1]), name='weight')
 b = tf.Variable(tf.random_normal([1]), name='bias')
-
-# sess = tf.Session()
-# sess.run(tf.global_variables_initializer())
-# print("w의 가중치: ", sess.run(w))
-# w의 가중치:  [2.2086694]
-
 h = x_train * w + b
 
-# 케라스 컴파일 시 가장 중요한 것 loss=(cost)
+#2-1. cost 손실 함수 정의
+# 케라스 컴파일 시, loss=(cost)를 줄여나가는 과정
 cost = tf.reduce_mean(tf.square(h - y_train))
-# (h-y)**2의 합 / 개수 == mse
+# (h-y)**2의 합 / 개수 ==> mse
 
-# 케라스 컴파일 시 옵티마이저(경사하강법). minimize(cost)=cost가 가장 적을 때 구하라
-train = tf.train.GradientDescentOptimizer(learning_rate=1.0).minimize(cost)
+#2-2. cost를 최소화하는 옵티마이저 정의. minimize(cost)=cost가 가장 적을 때 구하라
+train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
 
-# with 문 안에 sess가 포함
-# with tf.Session() as sess:
+#3. 훈련
 with tf.compat.v1.Session() as sess:
     # 전체 변수들이 싹 초기화
     sess.run(tf.global_variables_initializer())

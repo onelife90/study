d82ae705ad59e0cecfe24a7b33fa89a4e2e55541
jieun-kim-debug <onelife90@naver.com>
2020-07-09 20:55:49 +0900

    Update tf012_iris.py

--- tf/tf012_iris.py
+++ tf/tf012_iris.py
@@ -18,7 +18,7 @@ y_data = iris.target
 sess = tf.Session()
 y_data = tf.one_hot(y_data, depth=3).eval(session=sess)
 sess.close()
-print(y_data.shape)     # (150, 3)
+# print(y_data.shape)     # (150, 3)
 
 #1-2. train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, random_state=88, train_size=0.8)
@@ -47,7 +47,16 @@ acc = tf.reduce_mean(tf.cast(tf.equal(pred,y), dtype=tf.float32))
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
+
     for step in range(2001):
-        _, cost_val, _ = sess.run([opt, loss, acc], feed_dict={x:x_train, y:y_train})
+        _, h_val, cost_val = sess.run([opt, h, loss], feed_dict={x:x_train, y:y_train})
         if step%200==0:
-            print(step, "cost_val: \n" , cost_val, "\n pred: \n", acc)
+            print(step, "cost_val: \n" , cost_val)
+
+    # 다들 이거 어디서 가져온거지? 다시 파악 0709
+    # 실제로 실현되는 부분
+    correct_prediction = tf.equal(tf.argmax(h,1),tf.argmax(y,1))
+    
+    #정확도
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print("Accuracy:",sess.run(accuracy,feed_dict={x:x_test,y:y_test}))

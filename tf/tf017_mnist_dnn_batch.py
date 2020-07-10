3572084fd96394ffbab0e320d2800e000b579299
jieun-kim-debug <onelife90@naver.com>
2020-07-10 16:34:37 +0900

    Update tf017_mnist_dnn_batch.py

--- tf/tf017_mnist_dnn_batch.py
+++ tf/tf017_mnist_dnn_batch.py
@@ -84,52 +84,52 @@ cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(h), axis=1))
 opt = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
 
 #3. 훈련
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
 
-for epoch in range(training_epochs): # 15
-    avg_cost = 0
+    for epoch in range(training_epochs): # 15
+        avg_cost = 0
 
-    for i in range(total_batch):    # 600
-        start = i * batch_size
-        end = start + batch_size
-        # 배치사이즈 완성하시오 0~99/100~199/200~299/.../59900~59999 슬라이싱 사용
-        
-        # batch_xs, batch_ys = x_train[:100], y_train[:100]
-        # batch_xs, batch_ys = x_train[100:200], y_train[100:200]
-        
-        # batch_xs, batch_ys = x_train[i:batch_size], y_train[i:batch_size]
-        # batch_xs, batch_ys = x_train[i*batch_size:batch_size*(i+1)], y_train[i*batch_size:batch_size(i+2)]
-        
-        batch_xs, batch_ys = x_train[start:end], y_train[start:end]
+        for i in range(total_batch):    # 600
+            start = i * batch_size
+            end = start + batch_size
+            # 배치사이즈 완성하시오 0~99/100~199/200~299/.../59900~59999 슬라이싱 사용
+            
+            # batch_xs, batch_ys = x_train[:100], y_train[:100]
+            # batch_xs, batch_ys = x_train[100:200], y_train[100:200]
+            
+            # batch_xs, batch_ys = x_train[i:batch_size], y_train[i:batch_size]
+            # batch_xs, batch_ys = x_train[i*batch_size:batch_size*(i+1)], y_train[i*batch_size:batch_size(i+2)]
+            
+            batch_xs, batch_ys = x_train[start:end], y_train[start:end]
 
-        feed_dict = {x:batch_xs, y:batch_ys, keep_prob:0.7}
-        c, _ = sess.run([cost, opt], feed_dict=feed_dict)
-        avg_cost += c /total_batch
+            feed_dict = {x:batch_xs, y:batch_ys, keep_prob:0.7}
+            c, _ = sess.run([cost, opt], feed_dict=feed_dict)
+            avg_cost += c /total_batch
 
-    print('epoch: ', '%04d' %(epoch+1), "cost: {:.9f}".format(avg_cost))
-    # epoch:  0001 cost: 3.179819596
-    # epoch:  0002 cost: 1.863974968
-    # epoch:  0003 cost: 1.338062303
-    # epoch:  0004 cost: 1.086432286
-    # epoch:  0005 cost: 0.954928038
-    # epoch:  0006 cost: 0.858678305
-    # epoch:  0007 cost: 0.788396372
-    # epoch:  0008 cost: 0.737423236
-    # epoch:  0009 cost: 0.701132693
-    # epoch:  0010 cost: 0.668992661
-    # epoch:  0011 cost: 0.638948249
-    # epoch:  0012 cost: 0.618771437
-    # epoch:  0013 cost: 0.604265884
-    # epoch:  0014 cost: 0.580154929
-    # epoch:  0015 cost: 0.565156868
-print("훈련 끝!")
+        print('epoch: ', '%04d' %(epoch+1), "cost: {:.9f}".format(avg_cost))
+        # epoch:  0001 cost: 3.179819596
+        # epoch:  0002 cost: 1.863974968
+        # epoch:  0003 cost: 1.338062303
+        # epoch:  0004 cost: 1.086432286
+        # epoch:  0005 cost: 0.954928038
+        # epoch:  0006 cost: 0.858678305
+        # epoch:  0007 cost: 0.788396372
+        # epoch:  0008 cost: 0.737423236
+        # epoch:  0009 cost: 0.701132693
+        # epoch:  0010 cost: 0.668992661
+        # epoch:  0011 cost: 0.638948249
+        # epoch:  0012 cost: 0.618771437
+        # epoch:  0013 cost: 0.604265884
+        # epoch:  0014 cost: 0.580154929
+        # epoch:  0015 cost: 0.565156868
+        print("훈련 끝!")
 
-# tf.argmax(h,1)==예측값의 1(행)을 기준으로 최대값과 tf.argmax(y,1)==실제값의 1(행)을 기준으로 최대값이 같은 것을 pred로 정의
-pred = tf.equal(tf.argmax(h,1),tf.argmax(y,1))
+    # tf.argmax(h,1)==예측값의 1(행)을 기준으로 최대값과 tf.argmax(y,1)==실제값의 1(행)을 기준으로 최대값이 같은 것을 pred로 정의
+    pred = tf.equal(tf.argmax(h,1),tf.argmax(y,1))
 
-# 예측값을 실수형으로 캐스팅하여 차원을 모두 제거하고 평균을 낸 acc 정의
-acc = tf.reduce_mean(tf.cast(pred, tf.float32))
+    # 예측값을 실수형으로 캐스팅하여 차원을 모두 제거하고 평균을 낸 acc 정의
+    acc = tf.reduce_mean(tf.cast(pred, tf.float32))
 
-print("acc: ", sess.run(acc, feed_dict={x:x_test, y:y_test, keep_prob:0.9}))
-# acc:  0.8933a
+    print("acc: ", sess.run(acc, feed_dict={x:x_test, y:y_test, keep_prob:0.9}))
+    # acc:  0.8933

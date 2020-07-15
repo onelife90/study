45ddf53d86b1c8fcd7b4dc7baa1208e84f3dd7f4
jieun-kim-debug <onelife90@naver.com>
2020-07-15 15:16:29 +0900

    Update and rename fk002_pung.py to fk002_get.py

--- flask/fk002_get.py
+++ flask/fk002_get.py
@@ -6,7 +6,7 @@ def hello123():
     return "<h1>hello world</h1>"
 
 # ping을 주면 pong을 get해라
-# 즉, url 규칙을 받아 해당하는 규칙의 url로 요청이 들어온 경우 등록한 함수를 실행해라
+# 'GET' == url에 파라미터를 함께 넘기는 방식
 @app.route('/ping', methods=['GET'])
 def ping():
     return "<h1>pong</h1>"

82bdbbbc9a108cb686137e1a0a53287d67ab7ecd
jieun-kim-debug <onelife90@naver.com>
2020-09-17 12:46:15 +0900

    RS_params underscore2

--- Project/team/deep/binary_RS.py
+++ Project/team/deep/binary_RS.py
@@ -3,8 +3,6 @@ import glob
 import cv2
 import tensorflow as tf
 import numpy as np
-from natsort import natsorted
-from PIL import Image
 from keras.optimizers import Adam, RMSprop, SGD, Adadelta, Adagrad, Nadam
 from keras.layers import LeakyReLU
 from keras.preprocessing.image import ImageDataGenerator
@@ -12,12 +10,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import RandomizedSearchCV
-
-
-#images = [cv2.imread(file) for file in glob.glob("D:/project/data/binary/*.jpg")]
-#print(images)
-
-
+import dill as pickle
+from keras.utils import plot_model
 
 def load_train(train_path, image_size, classes):
     images = []
@@ -39,7 +33,7 @@ def load_train(train_path, image_size, classes):
             #cv2.destroyAllWindows() 
             image = image.astype(np.float32)
             image = np.multiply(image, 1.0 / 255.0)
-            image1 = image.reshape(224,224,1)
+            image1 = image.reshape(224,224)
             images.append(image1)
             label = np.zeros(len(classes))
             label[index] = 1.0
@@ -66,6 +60,7 @@ print(f"y: {result[1]}")
 x = result[0]
 y = result[1]
 
+
 '''
 x = np.array(result[0])
 y = np.array(result[1])
@@ -77,18 +72,12 @@ np.save('D:/project/data/y_data.npy', y)
 x = np.load('D:/project/data/x_data.npy')
 y = np.load('D:/project/data/y_data.npy')
 '''
-#1-3. train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)#, validation_split=0.2)
-
-print(f"x_train.shape: {x_train.shape}, x_test.shape: {x_test.shape}") 
-# x_train.shape: (1600, 224, 224, 1), x_test.shape: (400, 224, 224, 1)
-print(f"y_train.shape: {y_train.shape}, y_test.shape: {y_test.shape}")
-# y_train.shape: (1600, 2), y_test.shape: (400, 2)
 
 #2. cnn network modeling
 def build_model(drop=0.1, optimizer='adam', learning_rate=0.1, activation='relu'):
     inputs = tf.keras.Input(shape=(224,224,1), name='inputs')
     x = tf.keras.layers.Conv2D(32, kernel_size=(3, 3), padding='same', activation=activation)(inputs)
+    x = tf.keras.layers.Dropout(drop)
     x = tf.keras.layers.MaxPooling2D(pool_size=(2,2), strides=2)(x)
     x = tf.keras.layers.Conv2D(64, kernel_size=(3,3), padding='same', activation=activation)(x)
     x = tf.keras.layers.Dropout(drop)
@@ -101,34 +90,49 @@ def build_model(drop=0.1, optimizer='adam', learning_rate=0.1, activation='relu'
     outputs = tf.keras.layers.Dense(2, activation='sigmoid')(x)
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
     opt = optimizer(learning_rate=learning_rate)
-    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['acc'])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
     return model
-    
+
 model = tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=build_model, verbose=1)
-                    
+
 #2-1. set pipeline
 pipe = Pipeline([("scaler", StandardScaler()), ("model", model)])
+
+#1-3. train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)#, validation_split=0.2)
+
+print(f"x_train.shape: {x_train.shape}, x_test.shape: {x_test.shape}") 
+# x_train.shape: (1600, 224, 224, 1), x_test.shape: (400, 224, 224, 1)
+print(f"y_train.shape: {y_train.shape}, y_test.shape: {y_test.shape}")
+# y_train.shape: (1600, 2), y_test.shape: (400, 2)
+
+x_train = x_train.reshape(x_train.shape[0]*x_train.shape[1],x_train.shape[2])
+#y_train = y_train.reshape(y_train.shape[0]*y_train.shape[1],y_train.shape[2])
 
 #2-2. set params
 def create_hyperparameters():
     
-    batches = [16,32,64,128,256]
+    batches = [8,16,24,32,40]
     optimizers = [RMSprop, Adam, Adadelta, SGD, Adagrad, Nadam]
-    learning_rate = np.linspace(0.1,1.0,10).tolist()
+    learning_rate = np.linspace(0.1,0.5,5).tolist()
     dropout = np.linspace(0.1,0.5,5).tolist()
-    activation = ['tanh', 'relu', 'elu', "selu", "softmax", "sigmoid", LeakyReLU()]
+    activation = ['tanh', 'relu', 'elu', "selu", "softmax", "sigmoid"]#, LeakyReLU()]
 
     # return: {key, value}
-    return{"batch_size": batches, "optimizer":optimizers,
-           "learning_rate": learning_rate, "drop": dropout, 
-           "activation": activation}
+    return{"model__batch_size": batches, "model__optimizer":optimizers,
+           "model__learning_rate": learning_rate, "model__drop": dropout, 
+           "model__activation": activation}
 
-hyperparameters = create_hyperparameters()
+hyperparams = create_hyperparameters()
 
 #2-3. set RandomizedSearchCV
-search = RandomizedSearchCV(pipe, hyperparameters, cv=3, n_jobs=-1)
+search = RandomizedSearchCV(pipe, hyperparams, cv=3)#, n_jobs=-1)
 # params = search.get_params().keys()
 # print(f"params: {params}")
+
+x_train = x_train.reshape(-1*x_train.shape[1],x_train.shape[2])
+
+
 search.fit(x_train, y_train)
 # Check the list of available parameters with `estimator.get_params().keys()
 # params: dict_keys(['cv', 'error_score', 'estimator__memory', 'estimator__steps', 
@@ -138,7 +142,10 @@ search.fit(x_train, y_train)
 # 'pre_dispatch', 'random_state', 'refit', 'return_train_score', 'scoring', 'verbose'])`.
 # error solving..
 
-print(f"best_params of cnn network:\n {search.best_params_}")
+for param in search.get_params().keys():
+    print(f"param:\n{param}")
+
+print(f"best_params of cnn network:\n {search.best__params__}")
 # {'model__optimizer': 'rmsprop', 'model__drop': 0.2, 'model__batch_size': 128}
 acc = search.score(x_test, y_test)
 print(f"acc: {acc}")
@@ -146,7 +153,17 @@ print(f"acc: {acc}")
 
 
 
+#  plt.figure(figsize=(4.2, 4))
+#     for i, comp in enumerate(rbm.components_):
+#         plt.subplot(10, 10, i + 1)
+#         plt.imshow(comp.reshape((8, 8)), cmap=plt.cm.gray_r,
+#                    interpolation='nearest')
+#         plt.xticks(())
+#         plt.yticks(())
+#     plt.suptitle('64 components extracted by RBM', fontsize=16)
+#     plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
 
+#     plt.show()
 
 
 #'''
